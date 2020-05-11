d578ed9f11b3e195c791bec8696852f8730128f7
Jinlong <jinlong@Jinlongs-iMac.local>
2020-05-10 23:07:20 -0700

    added an example of backward DA.

--- eakf/DA_backward_example.py
+++ eakf/DA_backward_example.py
@@ -6,20 +6,21 @@ import networkx as nx
 from eakf import EAKF
 import time
 import pickle
+import pdb
 
-def forward_model(G, params, state0, T, dt_max, t_range):
+def backward_model(G, params, stateT0, T0, T, dt_max, t_range):
     model = MasterEqn(G)
     # Dimension of states: num_status * num_nodes, num_time_steps
-    states = model.solve(params, state0, T, dt_max, t_range)
+    states = model.backwards_solve(params, stateT0, T0, T, dt_max, t_range)
     return states.T
 
-def ensemble_forward_model(G, qi, xi, T, dt_max, t_range, parallel_flag = True):
+def ensemble_backward_model(G, qi, xi, T0, T, dt_max, t_range, parallel_flag = True):
     if parallel_flag == True:
         pool = multiprocessing.Pool(multiprocessing.cpu_count())
         results = []
         for iterN in range(qi.shape[0]):
-            results.append(pool.apply_async(forward_model, (G, qi[iterN,:], xi[iterN,:], T, dt_max, t_range)))
-        iterN = 0
+            results.append(pool.apply_async(backward_model, (G, qi[iterN,:], xi[iterN,:], T0, T, dt_max, t_range)))
+            iterN = 0
         states_all = np.zeros([qi.shape[0], len(t_range), xi.shape[1]])
         for result in results:
             states_all[iterN,:,:] = result.get()
@@ -28,15 +29,15 @@ def ensemble_forward_model(G, qi, xi, T, dt_max, t_range, parallel_flag = True):
     else:
         states_all = np.zeros([qi.shape[0], len(t_range), xi.shape[1]])
         for iterN in range(qi.shape[0]):
-            states_all[iterN,:,:] = forward_model(G, qi[iterN,:], xi[iterN,:], T, dt_max, t_range)
-    return states_all 
+            states_all[iterN,:,:] = backward_model(G, qi[iterN,:], xi[iterN,:], T0, T, dt_max, t_range)
+    return states_all
 
 def load_G():
     N = 1000
     G = nx.fast_gnp_random_graph(N, 5./(N-1))
     # G = nx.powerlaw_cluster_graph(N, 20, 5./(N-1))
 
-    edge_list = np.loadtxt('../notebooks/networks/High-School_data_2013.csv', usecols = [1,2], dtype = int)
+    edge_list = np.loadtxt('../data/networks/High-School_data_2013.csv', usecols = [1,2], dtype = int)
     # edge_list = np.loadtxt('networks/thiers_2012.csv', usecols = [1,2], dtype = int)
 
     G = nx.Graph([tuple(k) for k in edge_list])
@@ -58,7 +59,7 @@ if __name__ == "__main__":
     print("Number of cpu : ", multiprocessing.cpu_count())
 
     # Number of EAKF steps
-    steps_DA = 10
+    steps_DA = 10 
     # Ensemble size (required>=2)
     n_samples = 100
     # Number of status for each node
@@ -67,30 +68,38 @@ if __name__ == "__main__":
     # Load the true data (beta=0.04, T=100, dt=1)
     fin = 'data/states_truth_beta_0p04.pkl'
     data = pickle.load(open(fin, 'rb'))
+    data = np.fliplr(data)
     data = data.T
+
+    fin = 'data/g.pkl'
+    x_IC = pickle.load(open(fin, 'rb'))
 
     # Load network
     G, N = load_G()
 
     # Set prior for unknown parameters
     params = np.zeros([n_samples,1])
-    params[:,0] = np.random.uniform(np.log(0.01), np.log(0.5), n_samples)
+    params[:,0] = np.random.uniform(np.log(0.03), np.log(0.05), n_samples)
 
     # Set initial states
     states_IC = np.zeros([n_samples, n_status*N])
     for iterN in range(n_samples):
-        states_IC[iterN, :] = random_IC()
+        #states_IC[iterN, :] = random_IC()
+        states_IC[iterN, :] = x_IC[-1, iterN, :] 
 
     # Set time informations inside an EAKF step
     T = 10.0
+    Tinit = 0.0
     dt = 1.0 #timestep for OUTPUT not solver
     steps_T = int(T/dt)
     #OD: Are you sure you want to exclude T here? .
+    #t_range = np.linspace(0.0, T, num=steps_T+1, endpoint=True)#includes 0 and T
     t_range=np.arange(0.0,T,dt) 
     dt_fsolve =T/10.
+    dt_bsolve =T/10.
     
-    # Container for forward model evaluations
-    x_forward_all = np.empty([n_samples, steps_T*steps_DA, n_status*N])
+    # Container for backward model evaluations
+    x_backward_all = np.empty([n_samples, steps_T*steps_DA, n_status*N])
 
     ekf = EAKF(params, states_IC)
     for iterN in range(steps_DA):
@@ -106,26 +115,28 @@ if __name__ == "__main__":
         start = time.time()
         # A simple decayed noise term to add into paramters during EAKF
         params_noises = np.random.uniform(-2./np.sqrt(iterN+1), 2./np.sqrt(iterN+1), n_samples) 
-        x_forward = ensemble_forward_model(G, ekf.q[iterN] + params_noises.reshape(n_samples,1), states_IC,T, dt_fsolve, t_range, parallel_flag = True)
+        x_backward = ensemble_backward_model(G, ekf.q[iterN] + params_noises.reshape(n_samples,1), \
+                                             states_IC, \
+                                             T, Tinit, dt_bsolve, t_range, parallel_flag = True)
         end = time.time()
-        print('Time elapsed for forward model: ', end - start)
+        print('Time elapsed for backward model: ', end - start)
     
         ## EAKF to update joint states
         start = time.time()
-        ekf.update(x_forward[:,-1,:])
-        x_forward[:,-1,:] = ekf.x[-1]
-        x_forward_all[:,iterN*steps_T:(iterN+1)*steps_T,:] = x_forward
+        ekf.update(x_backward[:,-1,:])
+        x_backward[:,-1,:] = ekf.x[-1]
+        x_backward_all[:,iterN*steps_T:(iterN+1)*steps_T,:] = x_backward
         end = time.time()
         print('Time elapsed for EAKF: ', end - start)
-        states_IC = x_forward[:,-1,:]
+        states_IC = x_backward[:,-1,:]
         print("Error: ", ekf.error[-1])
 
         ## Output files 
         ## Overwrite for each EAKF step to facilitate debugging
-        pickle.dump(ekf.q, open("data/u.pkl", "wb"))
-        pickle.dump(ekf.x, open("data/g.pkl", "wb"))
-        pickle.dump(ekf.error, open("data/error.pkl", "wb"))
-        pickle.dump(x_forward_all, open("data/x.pkl", "wb"))
+        pickle.dump(ekf.q, open("data/u_back.pkl", "wb"))
+        pickle.dump(ekf.x, open("data/g_back.pkl", "wb"))
+        pickle.dump(ekf.error, open("data/error_back.pkl", "wb"))
+        pickle.dump(x_backward_all, open("data/x_back.pkl", "wb"))
 
 
         
