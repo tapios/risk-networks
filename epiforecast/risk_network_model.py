5e7fa2465645236bddf3e8a8697454a847f3622c
Gregory Wagner <wagner.greg@gmail.com>
2020-05-13 13:41:35 -0400

    Adds transmission rates to TranisitionRates

--- epiforecast/risk_network_model.py
+++ epiforecast/risk_network_model.py
@@ -7,7 +7,13 @@ def trivial_closure(infection_pressure): return None
 class StaticRiskNetworkModel:
     """A statistical epidemic model for the expected status of individuals
     on a static contact network."""
-    def __init__(self, N_nodes = 1, contact_network = None, transition_rates = None, infection_pressure_closure = trivial_closure):
+    def __init__(self, 
+                                         N_nodes = 1, 
+                                 contact_network = None, 
+                                transition_rates = None, 
+                      infection_pressure_closure = trivial_closure
+                 ):
+
         self.N_nodes = N_nodes
 
         # This method is used during time-integration to 'add' a closure term to the infection pressure
@@ -40,9 +46,9 @@ class StaticRiskNetworkModel:
             [None,                  None,              sps.diags(theta),  sps.diags(theta_prime),  None, None],
             [None,                  None,              sps.diags(mu),     sps.diags(mu_prime),     None, None]
 
-                                              ], format = 'csr'), shape = [6 * self.N_nodes, 6 * self.N_nodes])
+        ], format = 'csr'), shape = [6 * self.N_nodes, 6 * self.N_nodes])
 
-        self.transmission_matrix = np.array([transition_rates.beta_ij, self.beta_ij_prime])
+        self.transmission_matrix = np.array([transition_rates.beta_ij, self.beta_prime_ij])
 
     def set_contact_network(self, contact_network):
         """Set the contact network and convert contact network to a scipy sparse matrix."""
@@ -55,7 +61,8 @@ class StaticRiskNetworkModel:
 
         iS, iE, iI, iH = [range(j * self.N_nodes, (j + 1) * self.N_nodes) for j in range(4)]
 
-        self.infection_pressure = sps.kron(self.transmission_matrix, self.contact_matrix).dot(np.hstack([state[iI], state[iH]]))
+        self.infection_pressure = sps.kron(self.transmission_matrix, 
+                                           self.contact_matrix).dot(np.hstack([state[iI], state[iH]]))
 
         self.add_infection_pressure_closure(self.infection_pressure)
 
@@ -71,7 +78,8 @@ class StaticRiskNetworkModel:
 
         iS, iE, iI, iH = [range(j * self.N_nodes, (j + 1) * self.N_nodes) for j in range(4)]
 
-        self.infection_pressure = sps.kron(self.transmission_matrix, self.contact_matrix).dot(np.hstack([state[I], state[H]]))
+        self.infection_pressure = sps.kron(self.transmission_matrix, 
+                                           self.contact_matrix).dot(np.hstack([state[iI], state[iH]]))
 
         self.add_infection_pressure_closure(self.infection_pressure)
 
@@ -95,17 +103,21 @@ class TransitionRates:
     """A container for transition rates. """
     def __init__(self, 
                  # TODO: fill in sensible defaults
-                       exposed_to_infected = 0, # sigma
-                  infected_to_hospitalized = 0, # h * gamma
-                     infected_to_resistant = 0, # (1 - h - d) * gamma
-                      infected_to_deceased = 0, # d * gamma
-                 hospitalized_to_resistant = 0, # (1 - d_prime ) * gamma_prime
-                  hospitalized_to_deceased = 0, # d_prime * gamma_prime
+                               transmission_rate = 0, # sigma
+                      hospital_transmission_rate = 0, # sigma
+                             exposed_to_infected = 0, # sigma
+                        infected_to_hospitalized = 0, # h * gamma
+                           infected_to_resistant = 0, # (1 - h - d) * gamma
+                            infected_to_deceased = 0, # d * gamma
+                       hospitalized_to_resistant = 0, # (1 - d_prime ) * gamma_prime
+                        hospitalized_to_deceased = 0, # d_prime * gamma_prime
                   )
                        
-        self.exposed_to_infected       = self.sigma       = exposed_to_infected
-        self.infected_to_hospitalized  = self.delta       = infected_to_hospitalized
-        self.infected_to_resistant     = self.theta       = infected_to_resistant
-        self.infected_to_deceased      = self.mu          = infected_to_deceased
-        self.hospitalized_to_resistant = self.theta_prime = hospitalized_to_resistant
-        self.hospitalized_to_deceased  = self.mu_prime    = hospitalized_to_deceased
+        self.transmission_rate          = self.beta_ij       = transmission_rate
+        self.hospital_transmission_rate = self.beta_prime_ij = hospital_transmission_rate
+        self.exposed_to_infected        = self.sigma         = exposed_to_infected
+        self.infected_to_hospitalized   = self.delta         = infected_to_hospitalized
+        self.infected_to_resistant      = self.theta         = infected_to_resistant
+        self.infected_to_deceased       = self.mu            = infected_to_deceased
+        self.hospitalized_to_resistant  = self.theta_prime   = hospitalized_to_resistant
+        self.hospitalized_to_deceased   = self.mu_prime      = hospitalized_to_deceased
