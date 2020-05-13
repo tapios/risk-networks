d54e595fd5371291ca63abda15889863e8574c4a
Alfredo Garbuno <agarbuno@caltech.edu>
2020-05-13 14:57:40 -0700

    added comments on parametrization

--- risknet/ensemble.py
+++ risknet/ensemble.py
@@ -30,16 +30,15 @@ class epiens(object):
 			[member.init(beta = kwargs.get('beta', self.beta)) for member in self.ensemble]
 
 			for member in self.ensemble:
-				# Homogeneous parametrization:
-				member.sigma  =  member.sigma * np.random.uniform(0.9, 1.1, size = self.N)
-				member.delta  =  member.delta * np.random.uniform(0.9, 1.1, size = self.N)
-				member.theta  =  member.gamma * np.random.uniform(0.9, 1.1, size = self.N)
-				member.thetap =  member.gammap * np.random.uniform(0.9, 1.1, size = self.N)
-				member.mu     =  member.mu * np.random.uniform(0.9, 1.1, size = self.N)
-				member.mup    =  member.mup * np.random.uniform(0.9, 1.1, size = self.N)
-				member.gamma  =  (member.theta + member.delta + member.mu)
-				member.gammap =  (member.thetap + member.mup)
-
+				# Homogeneous parametrization (old, only used for prototyping):
+				member.sigma  =  member.sigma * np.random.uniform(0.9, 1.1, size = self.N)   # σ
+				member.delta  =  member.delta * np.random.uniform(0.9, 1.1, size = self.N)   # h γ
+				member.theta  =  member.gamma * np.random.uniform(0.9, 1.1, size = self.N)   # (1 - d - h) γ
+				member.thetap =  member.gammap * np.random.uniform(0.9, 1.1, size = self.N)  # (1 - d') γ'
+				member.mu     =  member.mu * np.random.uniform(0.9, 1.1, size = self.N)      # d  γ
+				member.mup    =  member.mup * np.random.uniform(0.9, 1.1, size = self.N)     # d' γ'
+				member.gamma  =  (member.theta + member.delta + member.mu)                   # γ
+				member.gammap =  (member.thetap + member.mup)                                # γ'
 		else:
 			self.__heterogeneous = True
 
@@ -81,11 +80,11 @@ class epiens(object):
 		# Initial conditions for all ensemble members Y0 = [y0, ..., y0]
 		y0 = np.copy(y)
 
-		Sidx, Eidx, Iidx, Hidx = [range(jj * member.N, (jj + 1) * member.N) for jj in range(4)]
+		iS, iE, iI, iH = [range(jj * member.N, (jj + 1) * member.N) for jj in range(4)]
 
-		member.beta_closure_ind = sps.kron(np.array([member.beta, member.betap]), member.L).dot(y0[Iidx[0]:(Hidx[-1]+1)])
+		member.beta_closure_ind = sps.kron(np.array([member.beta, member.betap]), member.L).dot(y0[iI[0]:(iH[-1]+1)])
 
-		y0[Sidx] = member.beta_closure_ind * y0[Sidx]
+		y0[iS] = member.beta_closure_ind * y0[iS]
 
 		return member.coeffs.dot(y0)
 
@@ -100,25 +99,25 @@ class epiens(object):
 		"""
 		member.y0 = np.copy(y)
 
-		Sidx, Eidx, Iidx, Hidx = [range(jj * member.N, (jj + 1) * member.N) for jj in range(4)]
+		iS, iE, iI, iH = [range(jj * member.N, (jj + 1) * member.N) for jj in range(4)]
 
 		# This makes the update:
 		# Y[S] = Y[S] * beta_closure_ind + beta_closure_cov
 		# where beta_closure_ind is based on Y[SI] = Y[S]·Y[I]
-		member.beta_closure_ind = sps.kron(np.array([member.beta, member.betap]), member.L).dot(member.y0[Iidx[0]:(Hidx[-1]+1)])
+		member.beta_closure_ind = sps.kron(np.array([member.beta, member.betap]), member.L).dot(member.y0[iI[0]:(iH[-1]+1)])
 
 		if kwargs.get('closure', 'individual') == 'covariance':
-			member.y0[Sidx] = member.beta_closure_ind * member.y0[Sidx] + self.beta_closure_cov
+			member.y0[iS] = member.beta_closure_ind * member.y0[iS] + self.beta_closure_cov
 		elif kwargs.get('closure', 'individual') == 'correlation':
-			member.y0[Sidx] = member.beta_closure_ind * member.y0[Sidx] + \
+			member.y0[iS] = member.beta_closure_ind * member.y0[iS] + \
 								self.beta_closure_cor[:, member_id]
 		else:
-			member.y0[Sidx] = member.beta_closure_ind * member.y0[Sidx]
+			member.y0[iS] = member.beta_closure_ind * member.y0[iS]
 
 		return member.coeffs.dot(member.y0)
 
 	def eval_closure(self, y, **kwargs):
-		Sidx, Eidx, Iidx, Hidx = [range(jj * self.N, (jj + 1) * self.N) for jj in range(4)]
+		iS, iE, iI, iH = [range(jj * self.N, (jj + 1) * self.N) for jj in range(4)]
 		self.L = self.ensemble[0].L
 		if kwargs.get('closure', 'individual') == 'covariance':
 			# This should be read as:
@@ -126,28 +125,28 @@ class epiens(object):
 			# Then we apply the Hadamard product with L (the contact matrix):
 			# L • Cov(Y[S], \beta Y[I] + \beta' Y[H])
 			self.beta_closure_cov = (1/(self.M-1)) * np.asarray(self.L.multiply(
-				y[:,Sidx].T.dot(self.PM).dot((self.beta * y[:,Iidx].T + self.betap * y[:,Hidx].T).T)).sum(axis = 1)
+				y[:,iS].T.dot(self.PM).dot((self.beta * y[:,iI].T + self.betap * y[:,iH].T).T)).sum(axis = 1)
 			).flatten()
 
 		elif kwargs.get('closure', 'individual') == 'correlation':
 			# Might not be optimal, but at least the equations are readable
-			self.covSI = y[:,Sidx].T.dot(self.PM).dot(y[:,Iidx].T.dot(self.PM).T)/(self.M-1)
-			self.varS = np.sqrt((y[:,Sidx].T.dot(self.PM) * y[:,Sidx].T.dot(self.PM)).sum(axis = 1)/(self.M-1))
-			self.varI = np.sqrt((y[:,Iidx].T.dot(self.PM) * y[:,Iidx].T.dot(self.PM)).sum(axis = 1)/(self.M-1))
+			self.covSI = y[:,iS].T.dot(self.PM).dot(y[:,iI].T.dot(self.PM).T)/(self.M-1)
+			self.varS = np.sqrt((y[:,iS].T.dot(self.PM) * y[:,iS].T.dot(self.PM)).sum(axis = 1)/(self.M-1))
+			self.varI = np.sqrt((y[:,iI].T.dot(self.PM) * y[:,iI].T.dot(self.PM)).sum(axis = 1)/(self.M-1))
 			self.denSI = self.varS.reshape(-1,1).dot(self.varI.reshape(1,-1))
 			self.LcorSI = self.L.multiply(self.covSI * (1/self.denSI))
 			self.LcorSI.data[np.isnan(self.LcorSI.data)] = 0.
 
-			self.covSH = y[:,Sidx].T.dot(self.PM).dot(y[:,Hidx].T.dot(self.PM).T)/(self.M-1)
-			self.varH = np.sqrt((y[:,Hidx].T.dot(self.PM) * y[:,Hidx].T.dot(self.PM)).sum(axis = 1)/(self.M-1))
+			self.covSH = y[:,iS].T.dot(self.PM).dot(y[:,iH].T.dot(self.PM).T)/(self.M-1)
+			self.varH = np.sqrt((y[:,iH].T.dot(self.PM) * y[:,iH].T.dot(self.PM)).sum(axis = 1)/(self.M-1))
 			self.denSH = self.varS.reshape(-1,1).dot(self.varH.reshape(1,-1))
 			self.LcorSH = self.L.multiply(self.covSH * (1/self.denSH))
 			self.LcorSH.data[np.isnan(self.LcorSH.data)] = 0.
 
 			#
-			self.beta_closure_cor = ((self.beta * self.LcorSI.dot(np.sqrt(y[:,Iidx] * (1-y[:,Iidx])).T) + \
- 									 self.betap * self.LcorSH.dot(np.sqrt(y[:,Hidx] * (1-y[:,Hidx])).T)) * \
-									       np.sqrt(y[:,Sidx] * (1-y[:,Sidx])).T)
+			self.beta_closure_cor = ((self.beta * self.LcorSI.dot(np.sqrt(y[:,iI] * (1-y[:,iI])).T) + \
+ 									 self.betap * self.LcorSH.dot(np.sqrt(y[:,iH] * (1-y[:,iH])).T)) * \
+									       np.sqrt(y[:,iS] * (1-y[:,iS])).T)
 
 	def set_solver(self, method = 'RK45', T = 200, dt = 0.1):
 
@@ -195,4 +194,3 @@ class epiens(object):
 			yt[:,jj + 1] = np.copy(self.y0.flatten())
 
 		return yt.reshape(self.M, -1, len(t))
-		
