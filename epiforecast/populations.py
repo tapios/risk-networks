afb5aaf41b4b24de01fcbe134e71ae801a7e847d
Gregory Wagner <wagner.greg@gmail.com>
2020-05-19 18:11:29 -0400

    Adds some docstrings

--- epiforecast/populations.py
+++ epiforecast/populations.py
@@ -1,6 +1,30 @@
 import numpy as np
 
 class ClinicalStatistics:
+    """
+    A container for clinical statistics.
+
+    Args
+    ----
+
+    ages (list-like): a list of age categories for the population
+
+    minimum: the minimum value of the statistic (note that this assumes 
+             `sampler.draw(age) is always greater than 0.)
+             
+    sampler: a 'sampler' with a function `sampler.draw(age)` that draws a random
+             sample from a distribution, depending on `age`. Samplers that are
+             age-independent must still support the syntax `sampler.draw(age)`. 
+
+    The six clinical statistics are
+
+    1. latent_period of infection (σ⁻¹)
+    2. community_infection_period over which infection persists in the 'community' (γ),
+    3. hospital_infection_period over which infection persists in a hospital setting (γ′),
+    4. hospitalization_fraction, the fraction of infected that become hospitalized (h),
+    5. community_mortality_fraction, the mortality rate in the community (d),
+    6. hospital_mortality_fraction, the mortality rate in a hospital setting (d′).
+    """
     def __init__(self, ages, minimum=0, sampler=None):
 
         if sampler is not None:
@@ -13,6 +37,11 @@ class ClinicalStatistics:
         self.minimum = minimum
 
 def populate_ages(population=1000, distribution=[0.25, 0.5, 0.25]):
+    """
+    Returns a numpy array of length `population`, with age categories from
+    0 to len(`distribution`), where the elements of `distribution` specify
+    the probability that an individual's age falls into the corresponding category.
+    """
     classes = len(distribution)
 
     ages = [np.random.choice(np.arange(classes), p=distribution) 
