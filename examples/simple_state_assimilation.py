0d1cda8d11a2c807a2e08338bede0c1b813244e4
Gregory Wagner <wagner.greg@gmail.com>
2020-05-25 11:11:15 -0400

    Rename assimilate observations to simple state assimilation

--- examples/simple_state_assimilation.py
+++ examples/simple_state_assimilation.py
@@ -15,37 +15,23 @@ n_status = 5
 noise_var = 0.01 # independent variance on observations
 n_samples = 2 # minimum number for an 'ensemble'
 
-#Determine which rates to assimilate
+observations = FullObservation(population, noise_var, "Full state observation 1% noise")
+assimilator = DataAssimilator(observations = observations, errors = [])
 
-
-
-
-#transition_rates_to_update_str = ['latent_periods',
-#                                  'community_infection_periods',
-#                                  'hospital_infection_periods',
-#                                  'hospitalization_fraction', 
-#                                  'community_mortality_fraction',
-#                                  'hospital_mortality_fraction']            
-                                  
-transition_rates_to_update_str=['community_infection_periods','community_mortality_fraction']                                  
-transmission_rate_to_update_flag = True
-
-assimilator = DataAssimilator(observations = FullObservation(population,noise_var,"Full state observation 1% noise"), 
-                              errors = [],
-                              transition_rates_to_update_str=transition_rates_to_update_str,
-                              transmission_rate_to_update_flag=transmission_rate_to_update_flag)
-
-# current state, transition_rates (20 of them), transmission rate (1)
+# Current state, transition_rates (20 of them), transmission rate (1)
 current_state = np.random.uniform(0.0, 1.0/6.0, (n_samples, population * n_status))
-print(np.sum(current_state[:,1::population],axis=0))
+
+print(np.sum(current_state[:, 1::population], axis=0))
 
 # For transition_rates see generate_clinical_statistics.py
-
 age_distribution = [ 1. ]
 ages = populate_ages(population, distribution=age_distribution)
 
-transition_rates=[]
+transmission_rate = np.random.uniform(0.03, 0.1, (n_samples, 1))
+transition_rates = []
+
 for i in range(n_samples):
+    #transition_rates.append(king_county_transition_rates(population))
 
     latent_periods = ClinicalStatistics(ages = ages, minimum = 2,
                                         sampler = GammaSampler(k=1.7, theta=2.0))
@@ -74,22 +60,17 @@ for i in range(n_samples):
 
     transition_rates.append(t_rates) 
     
-
-transmission_rate = np.random.uniform(0.03, 0.1, (n_samples,1))
- 
 # some data (corresponding to the size of the current state)
 synthetic_data = 1.0/6.0 * np.ones(population * n_status)
 
 # currently no implemented Observation classes rely upon this.
-contact_network=nx.watts_strogatz_graph(population,12,0.1,1)
-
+contact_network = nx.watts_strogatz_graph(population, 12, 0.1, 1)
 
 for i in np.arange(20):
-    #update states, the transition_rates object and the transmission rate array.
-    new_state, new_transition_rates, new_transmission_rate = assimilator.update(current_state,
-                                                                                 synthetic_data,
-                                                                                 full_ensemble_transition_rates=transition_rates,
-                                                                                 full_ensemble_transmission_rate=transmission_rate,
-                                                                                 contact_network=contact_network)
-    transmission_rate=new_transmission_rate
-    
+    # Update states, the transition_rates object and the transmission rate array.
+    new_state, new_transition_rates, new_transmission_rate = assimilator.update(current_state, synthetic_data,
+                                                                                  full_ensemble_transition_rates = transition_rates,
+                                                                                 full_ensemble_transmission_rate = transmission_rate,
+                                                                                                 contact_network = contact_network
+                                                                                )
+    transmission_rate = new_transmission_rate
