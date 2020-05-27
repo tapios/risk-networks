e339dd9adc283dbf92d8c80f2dedc18080e6cc32
lubo93 <lucasb@ethz.ch>
2020-05-27 10:47:06 -0700

    population without hospital beds

--- epiforecast/node_identifier_helper.py
+++ epiforecast/node_identifier_helper.py
@@ -5,5 +5,9 @@ def load_node_identifiers(self, filename=None):
    node_identifiers = np.loadtxt(filename, dtype = str)
    nodes = data[:,0].astype(np.int)
    identifiers = data[:,1]
+   
+   # number of nodes without hospital beds
+   pop = len(identifiers[identifiers != 'HOSP'])
+   
    # something has to be done with these identifiers later on in setIC
 
