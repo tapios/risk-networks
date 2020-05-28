08a5e6e53481fb40061b8b8f9de37cbc45fb458b
odunbar <odunbar@caltech.edu>
2020-05-28 21:41:04 +0100

    generate clinical rates for healthcare workers and general population

--- epiforecast/node_identifier_helper.py
+++ epiforecast/node_identifier_helper.py
@@ -1,13 +1,14 @@
-def load_node_identifiers(self, filename=None):
-   if filename is None:
-     filename = self.fallback_node_identifier_filename
-
-   node_identifiers = np.loadtxt(filename, dtype = str)
+import numpy as np
+def load_node_identifiers(filename):
+ 
+   data = np.loadtxt(filename, dtype = str)
+   
    nodes = data[:,0].astype(np.int)
    identifiers = data[:,1]
-   
-   # number of nodes without hospital beds
-   pop = len(identifiers[identifiers != 'HOSP'])
-   
-   # something has to be done with these identifiers later on in setIC
+
+   hospital_beds  = nodes[identifiers == 'HOSP']
+   health_workers = nodes[identifiers == 'HCW']
+   community      = nodes[identifiers == 'CITY']
+  
+   return hospital_beds, health_workers, community
 
--- examples/generate_clinical_parameters.py
+++ examples/generate_clinical_parameters.py
@@ -3,13 +3,24 @@
 
 # Get the epiforecast package onto our path:
 import os, sys; sys.path.append(os.path.join(".."))
+import numpy as np
 
 # Import utilities for generating random populations
 from epiforecast.populations import populate_ages, sample_clinical_distribution, TransitionRates
 from epiforecast.samplers import GammaSampler, AgeAwareBetaSampler
+from epiforecast.node_identifier_helper import load_node_identifiers
 
-# First we define the population by the number of individuals
-population = 100
+
+(
+hospital_beds,
+health_workers,
+community
+)= load_node_identifiers(os.path.join('..', 'data', 'networks', 'node_identifier_SBM_1e3.txt'))
+community_population = community.shape[0]
+health_worker_population = health_workers.shape[0]
+population = community_population + health_worker_population
+
+#Calculate the transition_rates for the community population:
 
 # ... and the age category of each individual
 age_distribution = [ 0.23,  # 0-19 years
@@ -18,23 +29,31 @@ age_distribution = [ 0.23,  # 0-19 years
                      0.079  # 65-75 years
                     ]
 
+#health workers sampled only from 20-64
+working_age_distribution= [ 0.39/(0.25+0.39), 0.25 / (0.25+0.39)] 
+
 # 75 onwards
 age_distribution.append(1 - sum(age_distribution))
 
-ages = populate_ages(population, distribution=age_distribution)
-
+community_ages = populate_ages(community_population, distribution=age_distribution)
+health_worker_ages = populate_ages(health_worker_population, distribution=working_age_distribution)
+ages = np.hstack([health_worker_ages,community_ages])
 # Print the ages we randomly generated:
 print("Ages categories of a random population:\n", ages, "\n")
 
 # Next, we randomly generate clinical properties for our example population.
 # Note that the units of 'periods' are days, and the units of 'rates' are 1/day.
+
+# Some parameters are independent of population classification (health_worker or community)
 latent_periods              = sample_clinical_distribution(GammaSampler(k=1.7, theta=2.0), population=population, minimum=2)
 community_infection_periods = sample_clinical_distribution(GammaSampler(k=1.5, theta=2.0), population=population, minimum=1)
 hospital_infection_periods  = sample_clinical_distribution(GammaSampler(k=1.5, theta=3.0), population=population, minimum=1)
 
+# Community parameters
 hospitalization_fraction     = sample_clinical_distribution(AgeAwareBetaSampler(mean=[ 0.02,  0.17,  0.25, 0.35, 0.45], b=4), ages=ages)
 community_mortality_fraction = sample_clinical_distribution(AgeAwareBetaSampler(mean=[0.001, 0.001, 0.005, 0.02, 0.05], b=4), ages=ages)
 hospital_mortality_fraction  = sample_clinical_distribution(AgeAwareBetaSampler(mean=[0.001, 0.001,  0.01, 0.04,  0.1], b=4), ages=ages)
+
 
 # Print the latent periods
 print("Randomly-generated latent periods:\n", latent_periods)
