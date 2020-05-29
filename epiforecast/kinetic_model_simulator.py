292fa0ce04a366e26a54252e3d9c231bd04e9d2e
Gregory Wagner <wagner.greg@gmail.com>
2020-05-29 12:41:24 -0400

    Resolves #26

--- epiforecast/kinetic_model_simulator.py
+++ epiforecast/kinetic_model_simulator.py
@@ -2,6 +2,14 @@ import numpy as np
 import networkx as nx
 from .simulation import Gillespie_simple_contagion
 
+def print_statuses(statuses):
+    nodes = len(statuses)
+
+    for i in range(nodes-1):
+        print(statuses[i], end=" ")
+
+    print(statuses[nodes-1])
+
 
 class KineticModel:
   def __init__(self,
@@ -34,12 +42,28 @@ class KineticModel:
                                              hospital_transmission_rate = community transmission_rate *
                                                                           hospital_transmission_reduction
     """
+
+    # Build networkx graph representing the contact network
+    self.edges = edges
+    self.contact_network = nx.Graph() # a static graph with {0,1} edges
+    self.contact_network.add_edges_from(edges)
+
+    self.community_transmission_rate = community_transmission_rate
+    self.hospital_transmission_rate = community_transmission_rate * hospital_transmission_reduction
+
+    self.set_mean_contact_duration(mean_contact_duration)
+
+    # Keep node identifiers 
+    self.node_identifiers = node_identifiers
+
+    # Will store the address of a patient in a hospital bed, by calling address_of_patient[hospital_bed]
+    self.address_of_patient = np.repeat(-1, node_identifiers["hospital_beds"].size)
+
+    # What statuses to return from Gillespie simulation
+    self.return_statuses = ('S', 'E', 'I', 'H', 'R', 'D', 'P')
+
     
-    #Build networkx graph
-    self.static_graph = nx.Graph() # a static graph with {0,1} edges
-    self.static_graph.add_edges_from(edges)
-    
-    # independent rates diagram
+    # Independent rates diagram
     self.diagram_indep = nx.DiGraph()
     self.diagram_indep.add_node('P') # placeholder compartment (hosp. beds)
     self.diagram_indep.add_node('S')
@@ -50,56 +74,35 @@ class KineticModel:
     self.diagram_indep.add_edge('H', 'R', rate=1, weight_label='hospitalized_to_resistant')
     self.diagram_indep.add_edge('H', 'D', rate=1, weight_label='hospitalized_to_deceased')
 
-    # neighbor-induced rates diagram
+    # Neighbor-induced rates diagram
     self.diagram_neigh = nx.DiGraph()
-    self.diagram_neigh.add_edge(
-      ('I','S'), ('I','E'), rate=1, weight_label='SI->E'
-    )
-    self.diagram_neigh.add_edge(
-      ('H','S'), ('H','E'), rate=1, weight_label='SH->E'
-    )
+    self.diagram_neigh.add_edge(('I', 'S'), ('I', 'E'), rate = self.community_transmission_rate, weight_label = 'SI->E')
+    self.diagram_neigh.add_edge(('H', 'S'), ('H', 'E'), rate = self.hospital_transmission_rate,  weight_label = 'SH->E')
+      
+    # Set the transition rates:    
+    nx.set_node_attributes(self.contact_network, values=transition_rates.exposed_to_infected,       name='exposed_to_infected')
+    nx.set_node_attributes(self.contact_network, values=transition_rates.infected_to_hospitalized,  name='infected_to_hospitalized')
+    nx.set_node_attributes(self.contact_network, values=transition_rates.infected_to_resistant,     name='infected_to_resistant')
+    nx.set_node_attributes(self.contact_network, values=transition_rates.infected_to_deceased,      name='infected_to_deceased')
+    nx.set_node_attributes(self.contact_network, values=transition_rates.hospitalized_to_resistant, name='hospitalized_to_resistant')
+    nx.set_node_attributes(self.contact_network, values=transition_rates.hospitalized_to_deceased,  name='hospitalized_to_deceased')
 
     
-    #set the transition rates:    
-    nx.set_node_attributes(self.static_graph, values=transition_rates.exposed_to_infected,       name='exposed_to_infected')
-    nx.set_node_attributes(self.static_graph, values=transition_rates.infected_to_hospitalized,  name='infected_to_hospitalized')
-    nx.set_node_attributes(self.static_graph, values=transition_rates.infected_to_resistant,     name='infected_to_resistant')
-    nx.set_node_attributes(self.static_graph, values=transition_rates.infected_to_deceased,      name='infected_to_deceased')
-    nx.set_node_attributes(self.static_graph, values=transition_rates.hospitalized_to_resistant, name='hospitalized_to_resistant')
-    nx.set_node_attributes(self.static_graph, values=transition_rates.hospitalized_to_deceased,  name='hospitalized_to_deceased')
-
-    #set the transmission rates
-    self.edges=edges
-    self.community_transmission_rate = community_transmission_rate
-    self.hospital_transmission_rate = community_transmission_rate * hospital_transmission_reduction
-    self.set_contact_network(mean_contact_duration)
-
-    # keep node identifiers 
-    self.node_identifiers=node_identifiers
-
-    #will store the address of a patient in a hospital bed, by calling address_of_patient[hospital_bed]
-    self.address_of_patient = np.repeat(-1,node_identifiers["hospital_beds"].size)
-
-    # what statuses to return from Gillespie simulation
-    self.return_statuses = ('S', 'E', 'I', 'H', 'R', 'D', 'P')
-
-  def set_contact_network(self, mean_contact_duration):
+  def set_mean_contact_duration(self, mean_contact_duration):
     """
-    Set the graph edge weights by multiplying the (community and hospital) transmission rates by the inputted `mean_contact_duration` and saving in a dict for
-    networkx
+    Set the weights of self.contact_network, which correspond to the mean contact
+    duration over a given time interval.
 
     Args
     ----
 
-    mean_contact_duration (np.array) : np.array of with [i,j] representing the edge between node i and j, holds the
+    mean_contact_duration (np.array) : np.array of with [i, j] representing the edge between node i and j, holds the
                                        mean duration that the edge was 'active' during an averaging window 
     """
-    community_network = {tuple(edge): self.community_transmission_rate * mean_contact_duration[edge[0], edge[1]]
-                         for edge in self.edges}
-    hospital_network  = {tuple(edge): self.hospital_transmission_rate * mean_contact_duration[edge[0], edge[1]]
-                         for edge in self.edges}
-    nx.set_edge_attributes(self.static_graph, values=community_network, name='SI->E')
-    nx.set_edge_attributes(self.static_graph, values=hospital_network, name='SH->E')
+    weights = {tuple(edge): mean_contact_duration[edge[0], edge[1]] for edge in self.edges}
+                         
+    nx.set_edge_attributes(self.contact_network, values = weights, name='SI->E')
+    nx.set_edge_attributes(self.contact_network, values = weights, name='SH->E')
     
   def simulate(self,
                node_statuses,
@@ -115,7 +118,7 @@ class KineticModel:
     static_contact_interval (float) : the integration time (over a static contact network)
     
     """
-    res = Gillespie_simple_contagion(self.static_graph,
+    res = Gillespie_simple_contagion(self.contact_network,
                                      self.diagram_indep,
                                      self.diagram_neigh,
                                      node_statuses,
