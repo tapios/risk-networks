ce42d2caa12298f9885c2c5fe4e121f733e1f048
Gregory Wagner <wagner.greg@gmail.com>
2020-06-02 01:18:58 -0400

    Still bug-riddled

--- sandbox/multithreaded_birth_death.py
+++ sandbox/multithreaded_birth_death.py
@@ -2,42 +2,56 @@ from numba import njit, prange, float64
 import numpy as np
 from timeit import default_timer as timer
 
+import matplotlib.pyplot as plt
+
 @njit
 def diurnal_inception_rate(λmin, λmax, t):
     return np.maximum(λmin, λmax * (1 - np.cos(np.pi * t)**4)**4)
 
 @njit
-def simulate_contact(stop_time, previous_stop_time, time, contact_duration, 
-                     overshoot_contact_duration, contact,
+def simulate_contact(stop_time,
+                     previous_stop_time,
+                     time,
+                     contact_duration, 
+                     overshoot,
+                     contact,
                      min_inception_rate,
                      max_inception_rate,
                      mean_contact_lifetime):
 
     if stop_time > time:
-        contact_duration = overshoot_contact_duration
+        contact_duration = overshoot
     else:
-        contact_duration = (stop_time - previous_stop_time) * contact
+        contact_duration = 0.0 #(stop_time - previous_stop_time) * contact
+        overshoot = time - stop_time
+
+    step = 0
 
     while time < stop_time:
+        step += 1
 
         inception_rate = diurnal_inception_rate(min_inception_rate, max_inception_rate, time)
 
-        if contact: # deactivate
+        if contact: # We have contact. Deactivate!
             time_step = - np.log(np.random.random()) * mean_contact_lifetime
-            contact_duration += time_step
-            contact = False
+
+            if time + time_step > stop_time:
+                contact_duration += stop_time - time
+            else:
+                contact_duration += time_step
+
             time += time_step
-        else: 
-            time += - np.log(np.random.random()) / inception_rate
+            contact = False
+
+        else: # Contact is absent. Proceed to activate.
+            time_step = - np.log(np.random.random()) / inception_rate
+
+            time += time_step
             contact = True
 
-    if contact:
-        overshoot_contact_duration = time - stop_time
-        contact_duration -= (time_step - overshoot_contact_duration)
-    else:
-        overshoot_contact_duration = 0.0
+    overshoot = (time - stop_time) * contact
 
-    return time, contact_duration, overshoot_contact_duration
+    return time, contact, contact_duration, overshoot
 
 
 @njit(parallel=True)
@@ -45,7 +59,7 @@ def simulate_contacts(stop_time,
                       previous_stop_time,
                       time,
                       contact_duration,
-                      overshoot_contact_duration,
+                      overshoot,
                       contact,
                       min_inception_rate,
                       max_inception_rate,
@@ -54,16 +68,17 @@ def simulate_contacts(stop_time,
     for i in prange(len(contact_duration)):
 
         (time[i],
+         contact[i],
          contact_duration[i],
-         overshoot_contact_duration[i]) = simulate_contact(stop_time,
-                                                           previous_stop_time,
-                                                           time[i],
-                                                           contact_duration[i],
-                                                           overshoot_contact_duration[i],
-                                                           contact[i],
-                                                           min_inception_rate[i],
-                                                           max_inception_rate[i],
-                                                           mean_contact_lifetime[i])
+         overshoot[i]) = simulate_contact(stop_time,
+                                          previous_stop_time,
+                                          time[i],
+                                          contact_duration[i],
+                                          overshoot[i],
+                                          contact[i],
+                                          min_inception_rate[i],
+                                          max_inception_rate[i],
+                                          mean_contact_lifetime[i])
                          
 
 if __name__ == "__main__":
@@ -74,29 +89,53 @@ if __name__ == "__main__":
     mean_contact_lifetime = 1 / 60 / 24 * np.ones(n)
     time = np.zeros(n)
     contact_duration = np.zeros(n)
-    overshoot_contact_duration = np.zeros(n)
-    contact = np.random.choice([False, True], size=n, p=[0.95, 0.05])
+    overshoot = np.zeros(n)
+    contacts = np.random.choice([False, True], size=n, p=[0.95, 0.05])
 
-    dt = 1 / 24
+    dt = 0.1 * 1 / 24
+    steps = int(2 / dt) 
     previous_stop_time = 0.0
 
     start = timer()
 
-    for i in range(24):
+    mean_contact_durations = []
+    active_contacts = []
+    times = []
 
-        simulate_contacts(
-                          (i + 1) * dt,
+    print(np.count_nonzero(contacts))
+
+    active_contacts.append(np.count_nonzero(contacts))
+    mean_contact_durations.append(contact_duration.mean())
+    times.append(0.0)
+
+    for i in range(steps):
+
+        simulate_contacts((i + 1) * dt,
                           i * dt,
                           time,
                           contact_duration,
-                          overshoot_contact_duration,
-                          contact,
+                          overshoot,
+                          contacts,
                           min_inception_rate,
                           max_inception_rate,
                           mean_contact_lifetime
                          )
 
+        active_contacts.append(np.count_nonzero(contacts))
+        mean_contact_durations.append(contact_duration.mean())
+        times.append((i+1) * dt)
+
     end = timer()
 
     print("Simulation time:", end - start)
     print("Mean contact duration:", contact_duration.mean())
+
+    fig, axs = plt.subplots(nrows=2)
+
+    plt.sca(axs[0])
+    plt.plot(times, active_contacts)
+
+    plt.sca(axs[1])
+    plt.plot(times[2:-1], mean_contact_durations[2:-1])
+
+    plt.show()
