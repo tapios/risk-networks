2bb882287855a1c98ff56763c37aa11d24b80ea3
Alfredo Garbuno <agarbuno@caltech.edu>
2020-06-03 10:41:37 -0700

    simple test for measurements

--- examples/simple_measurements.py
+++ examples/simple_measurements.py
@@ -1,7 +1,7 @@
 import os, sys; sys.path.append(os.path.join(".."))
 from epiforecast.epiplots import plot_master_eqns
 
-from epiforecast.populations import populate_ages, sample_pathological_distribution, TransitionRates
+from epiforecast.populations import populate_ages, sample_distribution, TransitionRates
 
 from epiforecast.samplers import GammaSampler, BetaSampler
 from epiforecast.samplers import GammaSampler, AgeDependentBetaSampler
@@ -9,6 +9,7 @@ from epiforecast.samplers import GammaSampler, AgeDependentBetaSampler
 from epiforecast.observations import FullObservation, HighProbRandomStatusObservation
 from epiforecast.data_assimilator import DataAssimilator
 from epiforecast.risk_simulator import MasterEquationModelEnsemble
+from epiforecast.measurements import TestMeasurements
 
 import numpy as np
 import networkx as nx
@@ -19,15 +20,15 @@ contact_network = nx.watts_strogatz_graph(population, 12, 0.1, 1)
 ensemble_size = 10
 
 
-latent_periods              = sample_pathological_distribution(GammaSampler(k=1.7, theta=2.0), population=population, minimum=2)
-community_infection_periods = sample_pathological_distribution(GammaSampler(k=1.5, theta=2.0), population=population, minimum=1)
-hospital_infection_periods  = sample_pathological_distribution(GammaSampler(k=1.5, theta=3.0), population=population, minimum=1)
+latent_periods              = sample_distribution(GammaSampler(k=1.7, theta=2.0), population=population, minimum=2)
+community_infection_periods = sample_distribution(GammaSampler(k=1.5, theta=2.0), population=population, minimum=1)
+hospital_infection_periods  = sample_distribution(GammaSampler(k=1.5, theta=3.0), population=population, minimum=1)
 
-hospitalization_fraction     = sample_pathological_distribution(BetaSampler(mean=0.25, b=4), population=population)
-community_mortality_fraction = sample_pathological_distribution(BetaSampler(mean=0.02, b=4), population=population)
-hospital_mortality_fraction  = sample_pathological_distribution(BetaSampler(mean=0.04, b=4), population=population)
+hospitalization_fraction     = sample_distribution(BetaSampler(mean=0.25, b=4), population=population)
+community_mortality_fraction = sample_distribution(BetaSampler(mean=0.02, b=4), population=population)
+hospital_mortality_fraction  = sample_distribution(BetaSampler(mean=0.04, b=4), population=population)
 
-transmission_rate = 0.06*np.ones(ens.M)
+transmission_rate = 0.06*np.ones(ensemble_size)
 transition_rates = TransitionRates(contact_network,
                                    latent_periods,
                                    community_infection_periods,
@@ -36,7 +37,50 @@ transition_rates = TransitionRates(contact_network,
                                    community_mortality_fraction,
                                    hospital_mortality_fraction)
 
-ensemble = MasterEquationModelEnsemble(contact_network,
+ensemble_model = MasterEquationModelEnsemble(contact_network,
                 [transition_rates]*ensemble_size,
                 transmission_rate,
                 ensemble_size = ensemble_size)
+
+np.random.seed(1)
+
+I_perc = 0.01
+y0      = np.zeros([ensemble_size, 5 * population])
+
+for mm, member in enumerate(ensemble_model.ensemble):
+    infected = np.random.choice(population, replace = False, size = int(population * I_perc))
+    E, I, H, R, D = np.zeros([5, population])
+    S = np.ones(population,)
+    I[infected] = 1.
+    S[infected] = 0.
+
+    y0[mm, : ]      = np.hstack((S, I, H, R, D))
+
+tF = 35
+res = ensemble_model.simulate(y0, tF, n_steps = 100)
+
+test = TestMeasurements()
+ode_states = res['states'][:,:,-1]
+
+test.update_prevalence(ode_states)
+
+def random_state(population):
+    """
+    Returns a status dictionary associated with a random infection
+    """
+    status_catalog = ['S', 'E', 'I', 'H', 'R', 'D']
+    status_weights = [0.8, 0.01, 0.15, 0.02, 0.01, 0.01]
+    statuses = {node: status_catalog[np.random.choice(6, p = status_weights)] for node in range(population)}
+
+    return statuses
+
+statuses = random_state(population)
+
+mean, var = test.take_measurements(statuses)
+
+print('Logit PPV (mean):\n')
+print(mean[mean != 0 ])
+print('Logit PPV (var):\n')
+print(var[mean != 0 ])
+
+mean, var = test.take_measurements(statuses, status = 'E')
