3669dbe35a42ac22536820279c0db1f50ea39716
odunbar <odunbar@caltech.edu>
2020-06-04 15:46:37 +0100

    added plotting

--- examples/specify_user_base.py
+++ examples/specify_user_base.py
@@ -2,56 +2,80 @@ import os, sys; sys.path.append(os.path.join(".."))
 
 import networkx as nx
 import numpy as np
+from matplotlib import pylab as pl
 
 from epiforecast.user_base import FullUserBase, FractionalUserBase, ContiguousUserBase, contiguous_indicators
 
 np.random.seed(123)
 
-contact_network = nx.watts_strogatz_graph(100000, 12, 0.1, 1)
+#plot graphs? NB plotting is very slow for >1000 nodes
+plotfigs=True
+
+contact_network = nx.watts_strogatz_graph(500, 20, 0.1, 1)
 population = len(contact_network)
 
 # create a full user base
 full_user_base=FullUserBase(contact_network)
 print("User base: Full")
-print("number of nodes", len(full_user_base.contact_network.nodes))
-print("number of edges", len(full_user_base.contact_network.edges))
+print("number of nodes:", len(full_user_base.contact_network.nodes))
+print("number of edges:", len(full_user_base.contact_network.edges))
 
 # create a user base from a random fraction of the population
-user_fraction = 0.1
+user_fraction = 0.3
 fractional_user_base = FractionalUserBase(contact_network,user_fraction)
 print("")
 print("User base: ", user_fraction, " fraction of nodes, randomly chosen")
-print("number of nodes", len(fractional_user_base.contact_network.nodes))
-print("number of edges", len(fractional_user_base.contact_network.edges))
+print("number of nodes:", len(fractional_user_base.contact_network.nodes))
+print("number of edges:", len(fractional_user_base.contact_network.edges))
 
 interior,boundary,mean_exterior_neighbors = contiguous_indicators(contact_network,fractional_user_base.contact_network)
 print("number of interior nodes:", interior)
-print("number of boundary nodes", boundary)
-print("average exterior neighbours of boundary node", mean_exterior_neighbors)
+print("number of boundary nodes:", boundary)
+print("average exterior neighbours of boundary node:", mean_exterior_neighbors)
 
 # create a user base from a Contiguous region about a random seed user (or a specified one)
 neighbor_user_base = ContiguousUserBase(contact_network,user_fraction, method="neighbor", seed_user=None)
 print("")
 print("User base:", user_fraction, " fraction of nodes, chosen using neighbor method")
-print("number of nodes", len(neighbor_user_base.contact_network.nodes))
-print("number of edges", len(neighbor_user_base.contact_network.edges))
+print("number of nodes:", len(neighbor_user_base.contact_network.nodes))
+print("number of edges:", len(neighbor_user_base.contact_network.edges))
 
 interior,boundary,mean_exterior_neighbors = contiguous_indicators(contact_network,neighbor_user_base.contact_network)
 print("number of interior nodes:", interior)
-print("number of boundary nodes", boundary)
-print("average exterior neighbours of boundary node", mean_exterior_neighbors)
+print("number of boundary nodes:", boundary)
+print("average exterior neighbours of boundary node:", mean_exterior_neighbors)
 
 
 # create a user base from a Contiguous region about a random seed user (or a specified one)
 clique_user_base = ContiguousUserBase(contact_network,user_fraction, method="clique", seed_user=None)
 print("")
 print("User base:", user_fraction, " fraction of nodes, chosen using clique method")
-print("number of nodes", len(clique_user_base.contact_network.nodes))
-print("number of edges", len(clique_user_base.contact_network.edges))
+print("number of nodes:", len(clique_user_base.contact_network.nodes))
+print("number of edges:", len(clique_user_base.contact_network.edges))
 
 interior,boundary,mean_exterior_neighbors = contiguous_indicators(contact_network,clique_user_base.contact_network)
 print("number of interior nodes:", interior)
-print("number of boundary nodes", boundary)
-print("average exterior neighbours of boundary node", mean_exterior_neighbors)
+print("number of boundary nodes:", boundary)
+print("average exterior neighbours of boundary node:", mean_exterior_neighbors)
 
+#plot graph
+if plotfigs:
+    pl.figure(1,figsize=(10, 10), dpi=300)
+    nx.draw_networkx(contact_network,                     node_color='k', with_labels=False, node_size=10, alpha=0.1)
+    nx.draw_networkx(neighbor_user_base.contact_network,  node_color='r', with_labels=False, node_size=10)
+    pl.title('neighborhood based contact network', fontsize=20)
+    pl.savefig('neighbor_network.png')
+
+    pl.figure(2,figsize=(10, 10), dpi=300)
+    nx.draw_networkx(contact_network,                     node_color='k', with_labels=False, node_size=10, alpha=0.1)
+    nx.draw_networkx(clique_user_base.contact_network,    node_color='r', with_labels=False, node_size=10)
+    pl.title('clique based contact network',fontsize=20)
+    pl.savefig('clique_network.png')
+
+    
+    pl.figure(3,figsize=(10, 10), dpi=300)
+    nx.draw_networkx(contact_network,                      node_color='k', with_labels=False, node_size=10, alpha=0.1)
+    nx.draw_networkx(fractional_user_base.contact_network, node_color='r', with_labels=False, node_size=10)
+    pl.title('random subset contact network',fontsize=20)
+    pl.savefig('random_network.png')
 
