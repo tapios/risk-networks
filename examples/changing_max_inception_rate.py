e78bb5aad269dfc96ec0280dcf368f25662f8954
Gregory Wagner <wagner.greg@gmail.com>
2020-06-07 10:35:07 -0400

    nice are plotting for changing the maximum inception rate

--- examples/changing_max_inception_rate.py
+++ examples/changing_max_inception_rate.py
@@ -12,12 +12,12 @@ np.random.seed(1234)
 minute = 1 / 60 / 24
 
 λnight = 3
-λday = 22
+λday = 40
 μ = 1.0 / minute
-n_contacts = 100000
-dt = 1 / 24 # days
-days = 7
-steps = int(7 / dt)
+n_contacts = 10000
+dt = 0.1 / 24 # days
+days = 10
+steps = int(days / dt)
 
 contact_network = nx.barabasi_albert_graph(int(n_contacts / 10), 10)
 
@@ -28,8 +28,8 @@ simulator = ContactSimulator(contact_network = contact_network,
                              start_time = -dt)
 
 # Generate a time-series of contact durations and average number of active contacts
-contact_durations = np.zeros((steps, 4))
-measured_inceptions = np.zeros((steps, 4))
+n_contacts = nx.number_of_edges(contact_network)
+contact_durations = np.zeros((steps, n_contacts))
 mean_contact_durations = np.zeros(steps)
 measurement_times = np.arange(start=0.0, stop=(steps+1)*dt, step=dt)
 
@@ -41,46 +41,44 @@ for i in range(steps):
 
     stop_time = (i + 1) * dt
 
-    if stop_time == 3:
-        λday = 5
-        nx.set_node_attributes(contact_network, values=λday, name="day_inception_rate")
+    if stop_time == 5:
+        nx.set_node_attributes(contact_network, values=5, name="day_inception_rate")
 
-    simulator.run(stop_time = (i + 1) * dt)
+    simulator.run(stop_time = stop_time)
 
-    mean_contact_durations[i] = simulator.contact_duration.mean() / dt
+    mean_contact_durations[i] = simulator.contact_duration.mean()
 
-    for j in range(contact_durations.shape[1]):
-        contact_durations[i, j] = simulator.contact_duration[j] / dt
+    contact_durations[i, :] = simulator.contact_duration
 
 end = timer()
 
 print("Simulated", nx.number_of_edges(contact_network),
       "contacts in {:.3f} seconds".format(end - start))
 
-fig, axs = plt.subplots(nrows=2, figsize=(14, 8), sharex=True)
+fig, axs = plt.subplots(nrows=2, figsize=(10, 4), sharex=True)
 
 plt.sca(axs[0])
-for j in range(contact_durations.shape[1]):
-    plt.plot(measurement_times[1:], contact_durations[:, j], '.', alpha=0.6,
-             label="Contact {}".format(j))
-
-plt.ylabel("Mean contact durations, $T_i$")
+for i in range(int(np.round(n_contacts/10))):
+    plt.plot(measurement_times[1:], contact_durations[:, i] / dt,
+             marker=".", mfc="xkcd:light pink", mec=None, alpha=0.02, markersize=0.1)
+             
+plt.plot(measurement_times[1:], mean_contact_durations / dt,
+         linestyle="-", linewidth=3, color="xkcd:sky blue", label="Ensemble mean $ \\langle T_i \\rangle $")
+         
+plt.ylabel("Specific contact duration, $T_i$")
 plt.legend(loc='upper right')
 
 plt.sca(axs[1])
-
-t = measurement_times
-λ = np.zeros(steps)
-for i in range(steps):
-    λ[i] = diurnal_inception_rate(λnight, λday, 1/2 * (t[i] + t[i+1]))
-
-plt.plot(t[1:], λ / (λ + μ), linestyle="-", linewidth=3, alpha=0.4, label="$ \lambda(t) / [ \mu + \lambda(t) ] $")
-
-plt.plot(measurement_times[1:], mean_contact_durations, linestyle="--", color="k", linewidth=1,
-         label="$ \\bar{T}_i(t) $")
+plt.plot(measurement_times[1:], mean_contact_durations / dt,
+         linestyle="-", linewidth=1, color="xkcd:sky blue")
 
 plt.xlabel("Time (days)")
-plt.ylabel("Ensemble-averaged $T_i$")
-plt.legend(loc='upper right')
+plt.ylabel("$ \\langle T_i \\rangle $")
+
+for ax in axs:
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+
+axs[0].spines["bottom"].set_visible(False)
 
 plt.show()
