786ba73e4a234e27ec522ce3ddadd3f6a62f01e3
Dima <dburov.190@gmail.com>
2020-06-12 18:12:49 -0700

    Delete non-reduced system code

--- epiforecast/risk_simulator.py
+++ epiforecast/risk_simulator.py
@@ -8,21 +8,15 @@ class NetworkCompartmentalModel:
     ODE representation of the SEIHRD compartmental model.
     """
 
-    def __init__(self,
-                 N,
-                 hospital_transmission_reduction = 0.25):
+    def __init__(self, N, hospital_transmission_reduction=0.25):
 
         self.hospital_transmission_reduction = hospital_transmission_reduction
         self.N = N
 
-    def set_parameters(self, transition_rates  = None,
-                             transmission_rate = None,
-                             ix_reduced        = True):
+    def set_parameters(self, transition_rates=None, transmission_rate=None):
         """
         Setup the parameters from the transition_rates container into the model
         instance.
-
-        The default behavior is the reduced model with 5 equations per node.
         """
 
         if transmission_rate is not None:
@@ -40,36 +34,20 @@ class NetworkCompartmentalModel:
             self.gamma  = self.theta  + self.mu  + self.delta
             self.gammap = self.thetap + self.mup
 
-            if ix_reduced:
-                # Reduced system with 5 equations ------------------------------
-                iS, iI, iH = [range(jj * self.N, (jj + 1) * self.N) for jj in range(3)]
-                self.coeffs = sps.csr_matrix(sps.bmat(
-                    [
-                        [-sps.eye(self.N),       None,                               None,                    None,                   None],
-                        [sps.diags(-self.sigma), sps.diags(-self.sigma -self.gamma), sps.diags(-self.sigma),  sps.diags(-self.sigma), sps.diags(-self.sigma)],
-                        [None,                   sps.diags(self.delta),              sps.diags(-self.gammap), None,                   None],
-                        [None,                   sps.diags(self.theta),              sps.diags(self.thetap),  None,                   None],
-                        [None,                   sps.diags(self.mu),                 sps.diags(self.mup),     None,                   None]
-                    ],
-                    format = 'csr'), shape = [5 * self.N, 5 * self.N])
-                self.offset = np.zeros(5 * self.N,)
-                self.offset[iI] = self.sigma
-                self.y_dot = np.zeros_like(5 * self.N,)
-
-            else:
-
-                # Full system with 6 equations ---------------------------------
-                self.coeffs = sps.csr_matrix(sps.bmat(
-                    [
-                        [-sps.eye(self.N), None,                   None,                   None,                    None, None],
-                        [ sps.eye(self.N), sps.diags(-self.sigma), None,                   None,                    None, None],
-                        [ None,            sps.diags( self.sigma), sps.diags(-self.gamma), None,                    None, None],
-                        [ None,            None,                   sps.diags(self.delta),  sps.diags(-self.gammap), None, None],
-                        [ None,            None,                   sps.diags(self.theta),  sps.diags(self.thetap),  None, None],
-                        [ None,            None,                   sps.diags(self.mu),     sps.diags(self.mup),     None, None]
-                    ],
-                    format = 'csr'), shape = [6 * self.N, 6 * self.N])
-                self.y_dot  = np.zeros_like(6 * self.N,)
+            # Reduced system with 5 equations ------------------------------
+            iS, iI, iH = [range(jj * self.N, (jj + 1) * self.N) for jj in range(3)]
+            self.coeffs = sps.csr_matrix(sps.bmat(
+                [
+                    [-sps.eye(self.N),       None,                               None,                    None,                   None],
+                    [sps.diags(-self.sigma), sps.diags(-self.sigma -self.gamma), sps.diags(-self.sigma),  sps.diags(-self.sigma), sps.diags(-self.sigma)],
+                    [None,                   sps.diags(self.delta),              sps.diags(-self.gammap), None,                   None],
+                    [None,                   sps.diags(self.theta),              sps.diags(self.thetap),  None,                   None],
+                    [None,                   sps.diags(self.mu),                 sps.diags(self.mup),     None,                   None]
+                ],
+                format = 'csr'), shape = [5 * self.N, 5 * self.N])
+            self.offset = np.zeros(5 * self.N,)
+            self.offset[iI] = self.sigma
+            self.y_dot = np.zeros_like(5 * self.N,)
 
     def update_transition_rates(self, new_transition_rates):
         """
@@ -90,14 +68,9 @@ class NetworkCompartmentalModel:
                            transmission_rate = new_transmission_rate)
 
 class MasterEquationModelEnsemble:
-    def __init__(self,
-                 contact_network,
-                 transition_rates,
-                 transmission_rate,
-                 ensemble_size = 1,
-                 hospital_transmission_reduction = 0.25,
-                 reduced_system = True,
-                 start_time = 0.0):
+    def __init__(self, contact_network, transition_rates, transmission_rate,
+                 ensemble_size=1, hospital_transmission_reduction=0.25,
+                 start_time=0.0):
         """
         Args:
         -------
@@ -110,7 +83,6 @@ class MasterEquationModelEnsemble:
         self.G = self.contact_network = contact_network
         self.M = self.ensemble_size = ensemble_size
         self.N = len(self.G)
-        self.ix_reduced = reduced_system
         self.start_time = start_time
 
         self.ensemble = []
@@ -122,13 +94,11 @@ class MasterEquationModelEnsemble:
             if isinstance(transition_rates, list):
                 member.set_parameters(
                         transition_rates  = transition_rates[mm],
-                        transmission_rate = transmission_rate[mm],
-                        ix_reduced = self.ix_reduced)
+                        transmission_rate = transmission_rate[mm])
             else:
                 member.set_parameters(
                         transition_rates  = transition_rates,
-                        transmission_rate = transmission_rate,
-                        ix_reduced = self.ix_reduced)
+                        transmission_rate = transmission_rate)
 
             member.id = mm
             self.ensemble.append(member)
@@ -202,41 +172,8 @@ class MasterEquationModelEnsemble:
 
         return member.y_dot
 
-    def do_step_full(self, t, y, member, closure = 'independent'):
-        """
-        Args:
-        --------
-        y (array): an array of dims (M times N_statuses times N_nodes)
-        t (array): times for ode solver
-
-        Returns:
-        --------
-        y_dot (array): lhs of master eqns
-        """
-        member.y0 = np.copy(y)
-        iS, iE, iI, iH = [range(jj * member.N, (jj + 1) * member.N) for jj in range(4)]
-
-        if closure == 'independent':
-            member.beta_closure = member.beta  * self.CM_SI[:, member.id] + \
-                                  member.betap * self.CM_SH[:, member.id]
-            member.y0[iS]    = member.beta_closure * member.y0[iS]
-        else:
-            member.beta_closure = sps.kron(np.array([member.beta, member.betap]), self.L).dot(y[iI[0]:(iH[-1]+1)])
-            member.y0[iS]    = member.beta_closure  * member.y0[iS]
-
-        member.y_dot = member.coeffs.dot(member.y0)
-        # if self.dt < 0:
-            # TOL = 1e-8
-            # member.y_dot[   (member.y_dot * self.dt < TOL - y)   & ((member.y_dot * self.dt < 0) & (y <  TOL) )] = 0.
-            # member.y_dot[ (member.y_dot * self.dt > 1 - TOL - y) & ((member.y_dot * self.dt > 0) & (y > 1-TOL))] = 0.
-
-        return member.y_dot
-
     def eval_closure(self, y, closure = 'independent'):
-        if self.ix_reduced:
-            iS,     iI, iH = [range(jj * self.N, (jj + 1) * self.N) for jj in range(3)]
-        else:
-            iS, iE, iI, iH = [range(jj * self.N, (jj + 1) * self.N) for jj in range(4)]
+        iS, iI, iH = [range(jj * self.N, (jj + 1) * self.N) for jj in range(3)]
 
         if closure == 'independent':
             self.numSI = y[:,iS].T.dot(y[:,iI])/(self.M)
@@ -269,10 +206,7 @@ class MasterEquationModelEnsemble:
                     total = n_steps):
             self.eval_closure(self.y0, closure = closure)
             for mm, member in enumerate(self.ensemble):
-                if self.ix_reduced:
-                    self.y0[mm] += self.dt * self.do_step(t, self.y0[mm], member, closure = closure)
-                else:
-                    self.y0[mm] += self.dt * self.do_step_full(t, self.y0[mm], member, closure = closure)
+                self.y0[mm] += self.dt * self.do_step(t, self.y0[mm], member, closure = closure)
                 self.y0[mm]  = np.clip(self.y0[mm], 0., 1.)
             yt[:,jj + 1] = np.copy(self.y0.flatten())
 
@@ -302,10 +236,7 @@ class MasterEquationModelEnsemble:
         for jj, time in tqdm(enumerate(t[:-1]), desc = 'Simulate backward', total = n_steps):
             self.eval_closure(self.y0, closure = closure)
             for mm, member in enumerate(self.ensemble):
-                if self.ix_reduced:
-                    self.y0[mm] += self.dt * self.do_step(t, self.y0[mm], member, closure = closure)
-                else:
-                    self.y0[mm] += self.dt * self.do_step_full(t, self.y0[mm], member, closure = closure)
+                self.y0[mm] += self.dt * self.do_step(t, self.y0[mm], member, closure = closure)
                 self.y0[mm]  = np.clip(self.y0[mm], 0., 1.)
             self.tf += self.dt
             yt[:,jj + 1] = np.copy(self.y0.flatten())
