8d232dd9a5e1eb58e267f84e4c8ab97bc86f8b3a
Dima <dburov.190@gmail.com>
2020-06-30 16:51:08 -0700

    Clean up (outside da-sandbox)

--- examples/saving_contact_networks_kinetic_and_master.py
+++ examples/saving_contact_networks_kinetic_and_master.py
@@ -30,22 +30,6 @@ from epiforecast.data_assimilator import DataAssimilator
 from epiforecast.utilities import seed_numba_random_state
 from epiforecast.epidemic_data_storage import StaticIntervalDataSeries
 from epiforecast.risk_simulator_initial_conditions import deterministic_risk
-# def deterministic_risk(population, initial_states, ensemble_size=1):
-    
-#     states_ensemble = np.zeros([ensemble_size, 5 * population])
-
-#     init_catalog = {'S': False, 'I': True}
-#     infected = np.array([init_catalog[status] for status in list(initial_states.values())])
-
-#     for mm in range(ensemble_size):
-#         E, I, H, R, D = np.zeros([5, population])
-#         S = np.ones(population,)
-#         I[infected] = 1.
-#         S[infected] = 0.
-
-#         states_ensemble[mm, : ] = np.hstack((S, I, H, R, D))
-
-#     return states_ensemble
 
 #
 # Set random seeds for reproducibility
