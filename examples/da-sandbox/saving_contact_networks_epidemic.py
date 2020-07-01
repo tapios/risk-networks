d19329e3c7ee9e24702aa379152ac81998308b29
Dima <dburov.190@gmail.com>
2020-06-30 20:36:06 -0700

    Use '_epidemic_initializer' in 'saving_contact...'

--- examples/da-sandbox/saving_contact_networks_epidemic.py
+++ examples/da-sandbox/saving_contact_networks_epidemic.py
@@ -1,132 +1,23 @@
-import os, sys; sys.path.append(os.path.join("../.."))
+#load the initialization for the epidemic etc.
+from _epidemic_initializer import *
 
-from timeit import default_timer as timer
-
-import networkx as nx
-import numpy as np
-import random
-import copy
-import matplotlib.pyplot as plt
-
-from numba import set_num_threads
-
-set_num_threads(1)
-
-from epiforecast.populations import  TransitionRates
-from epiforecast.samplers import AgeDependentConstant
-from epiforecast.contact_network import ContactNetwork
-from epiforecast.scenarios import random_epidemic
-from epiforecast.epiplots import plot_ensemble_states, plot_epidemic_data
 from epiforecast.risk_simulator import MasterEquationModelEnsemble
-from epiforecast.epidemic_simulator import EpidemicSimulator
-from epiforecast.health_service import HealthService
-from epiforecast.utilities import seed_three_random_states
-from epiforecast.epidemic_data_storage import StaticIntervalDataSeries
+from epiforecast.scenarios import random_epidemic
 from epiforecast.risk_simulator_initial_conditions import deterministic_risk
+from epiforecast.epiplots import plot_ensemble_states, plot_epidemic_data
 
 
-#
-# Set random seeds for reproducibility
-#
-
-seed = 942395
-seed_three_random_states(seed)
-
-
-#
-# Load an example network
-#
-
-edges_filename = os.path.join('../..', 'data', 'networks',
-                              'edge_list_SBM_1e3_nobeds.txt')
-groups_filename = os.path.join('../..', 'data', 'networks',
-                               'node_groups_SBM_1e3_nobeds.json')
-
-network = ContactNetwork.from_files(edges_filename, groups_filename)
-population = network.get_node_count()
-populace = network.get_nodes()
-
-
-start_time = 0.0
-minute = 1 / 60 / 24
-hour = 60 * minute
+start_time = epidemic_simulator.time
 simulation_length = 2
 print("We first create an epidemic for",
       simulation_length,
       "days, then we solve the master equations forward for this time")
-#
-# Clinical parameters of an age-distributed population
-#
-age_distribution =[0.21, 0.4, 0.25, 0.08, 0.06]
-health_workers_subset = [1, 2] # which age groups to draw from for h-workers
-assert sum(age_distribution) == 1.0
-network.draw_and_set_age_groups(age_distribution, health_workers_subset)
-
-# We process the clinical data to determine transition rates between each epidemiological state,
-
-latent_periods = 3.7
-community_infection_periods = 3.2
-hospital_infection_periods = 5.0
-hospitalization_fraction = AgeDependentConstant([0.002,  0.01,   0.04, 0.076,  0.16])
-community_mortality_fraction = AgeDependentConstant([ 1e-4,  1e-3,  0.001,  0.07,  0.015])
-hospital_mortality_fraction = AgeDependentConstant([0.019, 0.073,  0.193, 0.327, 0.512])
-
-transition_rates = TransitionRates(population = network.get_node_count(),
-                                   lp_sampler = latent_periods,
-                                  cip_sampler = community_infection_periods,
-                                  hip_sampler = hospital_infection_periods,
-                                   hf_sampler = hospitalization_fraction,
-                                  cmf_sampler = community_mortality_fraction,
-                                  hmf_sampler = hospital_mortality_fraction,
-                    distributional_parameters = network.get_age_groups()
-)
-
-transition_rates.calculate_from_clinical() 
-network.set_transition_rates_for_kinetic_model(transition_rates)
-
-community_transmission_rate = 12.0
-
-
-
-#
-# Set up the epidemic simulator and health service
-#
-health_service = HealthService(original_contact_network = network,
-                               health_workers = network.get_health_workers())
-
-mean_contact_lifetime=0.5*minute
-static_contact_interval = 3 * hour
-hospital_transmission_reduction = 0.1
-
-min_contact_rate = 2
-max_contact_rate = 22
-
-network.set_lambdas(min_contact_rate,max_contact_rate)
-
-epidemic_simulator = EpidemicSimulator(
-                 contact_network = network,
-     community_transmission_rate = community_transmission_rate,
- hospital_transmission_reduction = hospital_transmission_reduction,
-         static_contact_interval = static_contact_interval,
-           mean_contact_lifetime = mean_contact_lifetime,
-              day_inception_rate = max_contact_rate,
-            night_inception_rate = min_contact_rate,
-                  health_service = health_service,
-                      start_time = start_time
-                                      )
-
-#
-# Run the epidemic simulation and store the results
-#
-
-# Create storage for networks and data
-epidemic_data_storage = StaticIntervalDataSeries(static_contact_interval)
 
 # set up the initial conditions
-fraction_infected=0.01
+fraction_infected = 0.01
 statuses = random_epidemic(population,
                            populace,
-                           fraction_infected=0.01)
+                           fraction_infected)
 
 epidemic_simulator.set_statuses(statuses)
 
@@ -136,10 +27,8 @@ time_trace = np.arange(time,simulation_length,static_contact_interval)
 statuses_sum_trace = [[population-int(fraction_infected*population), 0, int(fraction_infected*population),0,0,0]]
 
 
-fig, axes = plt.subplots(1, 3, figsize = (16, 4))
-# First we run and save the epidemic
+# Run the epidemic simulation and store the results
 for i in range(int(simulation_length/static_contact_interval)):
-    
     network = epidemic_simulator.run(stop_time = epidemic_simulator.time + static_contact_interval,
                                      current_network = network)
     #save the start time network and statuses
@@ -148,11 +37,10 @@ for i in range(int(simulation_length/static_contact_interval)):
 
     #update the statuses and time
     statuses = epidemic_simulator.kinetic_model.current_statuses
-    time=epidemic_simulator.time
+    time = epidemic_simulator.time
 
     #save the statuses at the new time
     epidemic_data_storage.save_end_statuses_to_network(end_time=time, end_statuses=statuses)
-
 
     statuses_sum_trace.append([epidemic_simulator.kinetic_model.statuses['S'][-1],
                                epidemic_simulator.kinetic_model.statuses['E'][-1],
@@ -160,7 +48,8 @@ for i in range(int(simulation_length/static_contact_interval)):
                                epidemic_simulator.kinetic_model.statuses['H'][-1],
                                epidemic_simulator.kinetic_model.statuses['R'][-1],
                                epidemic_simulator.kinetic_model.statuses['D'][-1]]) 
-    
+
+fig, axes = plt.subplots(1, 3, figsize = (16, 4))
 axes = plot_epidemic_data(kinetic_model = epidemic_simulator.kinetic_model,
                           statuses_list = statuses_sum_trace,
                                    axes = axes,
