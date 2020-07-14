b9cfcb6b75b4dc198015b1a24de7dddad10fa101
Dima <dburov.190@gmail.com>
2020-07-13 22:27:51 -0700

    Rewrite sbatch script to launch jobs in parallel

--- examples/da-sandbox/SBATCH_accuracy_estimation.sh
+++ examples/da-sandbox/SBATCH_accuracy_estimation.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 
-#SBATCH --time=12:00:00                 # walltime
-#SBATCH --ntasks=5                      # number of processor cores (i.e. tasks)
+#SBATCH --time=6:00:00                  # walltime
+#SBATCH --ntasks=1                      # number of processor cores (i.e. tasks)
 #SBATCH --mem-per-cpu=6G
 #SBATCH -J "risk_networks_accuracy"
-#SBATCH --output="output/slurm.out"
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
+#SBATCH --array=0-4
 
 
-# Submit this script with: sbatch --mail-user=yourmail@domain.com run_accuracy_estimation.sh
+# submit with: sbatch --mail-user=mail@domain.com SBATCH_accuracy_estimation.sh
 
-# main
+# preface ######################################################################
 set -euo pipefail
 
-module load python3/3.7.0
+OUTPUT_DIR_PREFIX="output"
+COUNTER=""
 
-python3 accuracy_estimation_script.py
+while [ -d "${OUTPUT_DIR_PREFIX}${COUNTER}" ]; do
+  ((COUNTER+=1))
+done
+
+OUTPUT_DIR="${OUTPUT_DIR_PREFIX}${COUNTER}"
+
+
+# parameters & constants #######################################################
+user_fractions=(0.03 0.05 0.1 0.5 1.0)
+fractions_tested=(0.5 0.2 0.1 0.02 0.01)
+
+user_fraction=${user_fractions[${SLURM_ARRAY_TASK_ID}]}
+tested=${fractions_tested[${SLURM_ARRAY_TASK_ID}]}
+output_path="${OUTPUT_DIR}/${user_fraction}"
+stdout="${output_path}/stdout"
+stderr="${output_path}/stderr"
+
+mkdir -p "${output_path}"
+
+
+# launch #######################################################################
+module load python3/3.7.0
+python3 accuracy_estimation.py \
+  --user-network-user-fraction=${user_fraction} \
+  --constants-output-path=${output_path} \
+  --observations-I-fraction-tested=${tested} \
+  >${stdout} 2>${stderr}
 
 
