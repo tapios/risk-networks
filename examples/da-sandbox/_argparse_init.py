23f8242e434649be0dca6468313adddd6484fafc
Dima <dburov.190@gmail.com>
2020-07-13 23:09:39 -0700

    Add 'network-node-count' CLI argument

--- examples/da-sandbox/_argparse_init.py
+++ examples/da-sandbox/_argparse_init.py
@@ -10,12 +10,15 @@ parser = argparse.ArgumentParser()
 
 print_info_module(__name__, "parsing args of PID:", os.getpid())
 
+# constants ####################################################################
+parser.add_argument('--constants-output-path', type=str, default='')
+
+# network ######################################################################
+parser.add_argument('--network-node-count', type=str, default='1e3')
+
 # user_network #################################################################
 parser.add_argument('--user-network-user-fraction', type=float, default=1.0)
 parser.add_argument('--user-network-seed-user', type=int, default=190)
-
-# constants ####################################################################
-parser.add_argument('--constants-output-path', type=str, default="")
 
 # observations #################################################################
 parser.add_argument('--observations-I-fraction-tested', type=float, default=0.1)
