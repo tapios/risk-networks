ae0d723a4e89233bc9ce242ece6fe8bdf369f7d3
Dima <dburov.190@gmail.com>
2020-07-13 20:31:15 -0700

    Update sbatch script appropriately

--- examples/da-sandbox/SBATCH_accuracy_estimation.sh
+++ examples/da-sandbox/SBATCH_accuracy_estimation.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH --time=12:00:00                 # walltime
-#SBATCH --ntasks=6                      # number of processor cores (i.e. tasks)
-#SBATCH --mem-per-cpu=1G
+#SBATCH --ntasks=5                      # number of processor cores (i.e. tasks)
+#SBATCH --mem-per-cpu=6G
 #SBATCH -J "risk_networks_accuracy"
 #SBATCH --output="output/slurm.out"
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
 
-# Submit this script with: sbatch run_accuracy_estimation.sh
+# Submit this script with: sbatch --mail-user=yourmail@domain.com run_accuracy_estimation.sh
 
 # main
 set -euo pipefail
