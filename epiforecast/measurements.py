Expt: choose Classifier thresholds for Infectious test , [Also fixes prevalence calculation] (#167)
* roc runs for different infectiousness testing, and how to read the output files, NOT SAVING FULL DATA YET

* import plots

* extend to single ensemble member

* classifier saves mean data and statuses, roc_from_data loads them

* renamed and added label to ROC plots

* comments in run test

* added new plotting and performance measure for true pos rate vs predicted pos fraction

* added
markers to plots

* correctly calculate point prevalence

* noisy measurements should be a default

* remove prints

* sens 0.99 default

* restructure DA loop

* further merge resolution

* plotting for ordered infectiousness plots

* added timers/print messages

* random ICs, with range of fractions infected

* change default I_min threshold

* typo

* organising experiments and scripts

* EXP_NAME in test script

* name change

* typo

Co-authored-by: odunbar <odunbar@caltech.edu>

epiforecast/measurements.py
@@ -6,7 +6,7 @@ class TestMeasurement:
             status,
             sensitivity=0.80,
             specificity=0.99,
-            noisy_measurement=False):
+            noisy_measurement=True):
 
         self.sensitivity = sensitivity
         self.specificity = specificity
@@ -24,12 +24,16 @@ class TestMeasurement:
         Inputs:
         -------
             ensemble_states : `np.array` of shape (ensemble_size, num_status * population) at a given time
-            status_idx      : status id of interest. Following the ordering of the reduced system SIHRD.
         """
         if fixed_prevalence is None:
-            population      = ensemble_states.shape[1]/self.n_status
+            population      = int(ensemble_states.shape[1]/self.n_status)
             ensemble_size   = ensemble_states.shape[0]
-            self.prevalence = ensemble_states.reshape(ensemble_size,self.n_status,-1)[:,self.status_catalog[self.status],:].sum(axis = 1)/population
+
+            prevalence = ensemble_states.reshape(ensemble_size,self.n_status, population)[:,self.status_catalog[self.status],:].sum(axis = 1) / float(population)
+            if ensemble_size > 1:
+                prevalence = np.mean(prevalence, axis=0)
+            
+            self.prevalence = max(prevalence, 1.0 / float(population)) * np.ones(ensemble_size)
         else:
             self.prevalence = fixed_prevalence
 
@@ -213,7 +217,7 @@ class HighVarianceStateInformedObservation:
             dec_sort_vector = np.argsort(-xvar)
 
             self.obs_states=candidate_states[dec_sort_vector[:obs_states_size]]
-
+            
         elif (self.obs_frac == 1.0):
             self.obs_states=candidate_states
         else: #The value is too small
@@ -232,7 +236,7 @@ class Observation(StateInformedObservation, TestMeasurement):
             max_threshold=1.0,
             sensitivity=0.80,
             specificity=0.99,
-            noisy_measurement=False,
+            noisy_measurement=True,
             obs_var_min = 1e-3):
 
         self.name=obs_name
@@ -309,7 +313,7 @@ class HighVarianceObservation(HighVarianceStateInformedObservation, TestMeasurem
             obs_name,
             sensitivity=0.80,
             specificity=0.99,
-            noisy_measurement=False,
+            noisy_measurement=True,
             obs_var_min = 1e-3):
 
         self.name=obs_name
@@ -369,7 +373,7 @@ class HighVarianceObservation(HighVarianceStateInformedObservation, TestMeasurem
 
         observed_mean     = np.array([mean[node] for node in observed_nodes])
         observed_variance = np.array([np.maximum(var[node], self.obs_var_min) for node in observed_nodes])
-
+        
         self.mean     = observed_mean
         self.variance = observed_variance
 

epiforecast/performance_metrics.py
@@ -22,6 +22,10 @@ def confusion_matrix(data,
 
     status_catalog = dict(zip(['S', 'E', 'I', 'H', 'R', 'D'], np.arange(6)))
     status_of_interest = np.array([status_catalog[status] for status in statuses])
+    if ensemble_states.ndim == 1:
+        #in the case of "1" ensemble member - ensure array is 2 dimensional
+        ensemble_states = np.array([ensemble_states])
+
     ensemble_size = len(ensemble_states)
     population    = len(data)
 
@@ -30,7 +34,7 @@ def confusion_matrix(data,
     #obtain the prediction of the ensemble by averaging
     ensemble_probabilities[ [0, 2, 3, 4, 5] ] = ensemble_states.reshape(ensemble_size, 5, population).mean(axis = 0)
     ensemble_probabilities[1] = 1 - ensemble_probabilities.sum(axis = 0)
-    
+   
     #obtain a binary classification of the prediction
     if method == 'sum':
          #if the sum of the statuses of interest > threshold then we assign true
@@ -51,6 +55,59 @@ def confusion_matrix(data,
     return skm.confusion_matrix(data_statuses, ensemble_statuses, labels = labels)
 
 
+class PredictedNegativeFraction:
+    """
+    Container for the Predicted Negative Fraction, based on overall class assignment
+    Predicted Negative Fraction = True Negatives + False Negatives/ Total
+    """
+
+    def __init__(self, name = 'PredictedNegativeFraction'):
+        self.name = name
+
+    def __call__(self,
+                 data,
+                 ensemble_states,
+                 statuses = ['S', 'E', 'I', 'H', 'R', 'D'],
+                 threshold = 0.5,
+                 method = 'or'):
+        """
+        Args:
+        -----
+                data           : dictionary with {node : status}
+                ensemble_state : (ensemble size, 5 * population) `np.array` with probabilities
+                statuses       : statuses of interest.
+        """
+        cm = confusion_matrix(data, ensemble_states, statuses, threshold, method)
+        tn, fp, fn, tp = cm.ravel()
+
+        return (tn + fn) / (tn + fn + tp + fp)
+
+class PredictedPositiveFraction:
+    """
+    Container for the Predicted Postive Fraction, based on overall class assignment
+    Predicted Postive Fraction  = (True Positives + False Positives) / Total
+    """
+
+    def __init__(self, name = 'PredictedPositiveFraction'):
+        self.name = name
+
+    def __call__(self,
+                 data,
+                 ensemble_states,
+                 statuses = ['S', 'E', 'I', 'H', 'R', 'D'],
+                 threshold = 0.5,
+                 method = 'or'):
+        """
+        Args:
+        -----
+                data           : dictionary with {node : status}
+                ensemble_state : (ensemble size, 5 * population) `np.array` with probabilities
+                statuses       : statuses of interest.
+        """
+        cm = confusion_matrix(data, ensemble_states, statuses, threshold, method)
+        tn, fp, fn, tp = cm.ravel()
+
+        return (tp + fp) / (tn + fn + tp + fp)
 
 class Accuracy:
     """

examples/da-sandbox/backward_forward_assimilation.py
@@ -7,9 +7,9 @@ from matplotlib import pyplot as plt
 from epiforecast.user_base import FullUserGraphBuilder
 from epiforecast.data_assimilator import DataAssimilator
 from epiforecast.time_series import EnsembleTimeSeries
-from epiforecast.risk_simulator_initial_conditions import kinetic_to_master_same_fraction
-from epiforecast.epiplots import plot_ensemble_states
-from epiforecast.performance_metrics import PerformanceTracker
+from epiforecast.risk_simulator_initial_conditions import kinetic_to_master_same_fraction, random_risk_range
+from epiforecast.epiplots import plot_roc_curve, plot_ensemble_states
+from epiforecast.performance_metrics import TrueNegativeRate, TruePositiveRate, PerformanceTracker
 from epiforecast.utilities import dict_slice
 
 
@@ -48,6 +48,7 @@ from _user_network_init import user_network, user_nodes, user_population
 
 # observations #################################################################
 from _observations_init import (random_infection_test,
+                                high_var_infection_test,
                                 positive_hospital_records,
                                 negative_hospital_records,
                                 positive_death_records,
@@ -59,7 +60,7 @@ perfect_observations   = [positive_hospital_records,
                           positive_death_records,
                           negative_death_records]
 
-I_observation_delay = 1.0
+I_observation_delay = 0.0
 
 # assimilator ##################################################################
 transition_rates_to_update_str   = []
@@ -74,6 +75,7 @@ assimilator_imperfect_observations = DataAssimilator(
 assimilator_perfect_observations = DataAssimilator(
         observations=perfect_observations,
         errors=[],
+        n_assimilation_batches = 4,
         transition_rates_to_update_str=transition_rates_to_update_str,
         transmission_rate_to_update_flag=transmission_rate_to_update_flag)
 
@@ -106,19 +108,17 @@ plt.savefig(os.path.join(OUTPUT_PATH, 'epidemic.png'), rasterized=True, dpi=150)
 ################################################################################
 # constants ####################################################################
 
-# floats/ints
-da_window         = 5.0
+da_window         = 7.0
 prediction_window = 1.0
 HD_assimilation_interval = 1.0 # assimilate H and D data every .. days
 I_assimilation_interval  = 1.0 # same for I
 
-# ints
-n_prediction_windows_spin_up = 7
-n_prediction_windows         = int(total_time/prediction_window)
-steps_per_da_window          = int(da_window/static_contact_interval)
-steps_per_prediction_window  = int(prediction_window/static_contact_interval)
+n_prediction_windows_spin_up = 12
+n_prediction_windows        = int(total_time/prediction_window)
+steps_per_da_window         = int(da_window/static_contact_interval)
+steps_per_prediction_window = int(prediction_window/static_contact_interval)
 
-assert n_prediction_windows_spin_up * prediction_window > da_window
+assert n_prediction_windows_spin_up * prediction_window + prediction_window > da_window 
 assert n_prediction_windows > n_prediction_windows_spin_up
 
 # storing ######################################################################
@@ -131,14 +131,13 @@ loaded_data = epidemic_data_storage.get_network_from_start_time(
         start_time=start_time)
 loaded_kinetic_ic = loaded_data.start_statuses
 
-ensemble_ic = kinetic_to_master_same_fraction(user_nodes,
-                                              loaded_kinetic_ic,
-                                              ensemble_size)
+ensemble_ic = random_risk_range(population,
+                                0.001,
+                                0.01,
+                                ensemble_size)
 
 master_eqn_ensemble.set_states_ensemble(ensemble_ic)
 master_eqn_ensemble.set_start_time(start_time)
-
-
 
 ################################################################################
 # master equations + data assimilation computation #############################
@@ -151,7 +150,7 @@ ensemble_state = ensemble_ic
 timer_spin_up = timer()
 print_info("Spin-up started")
 for j in range(spin_up_steps):
-    master_states_timeseries.push_back(ensemble_state) # store
+    master_states_timeseries.push_back(ensemble_state) # storage
 
     loaded_data = epidemic_data_storage.get_network_from_start_time(
             start_time=current_time)
@@ -169,9 +168,24 @@ for j in range(spin_up_steps):
     current_time += static_contact_interval
 
 print_info("Spin-up ended; elapsed:", timer() - timer_spin_up, end='\n\n')
+print_info("Spin-up ended: current time", current_time)
 
 # main loop: backward/forward/data assimilation ################################
+# 3 stages per loop: 
+# 1) prediction (no assimilation) forwards steps_per_prediction_window
+#    - Save data during this window from [ start , end )
+# 2) backward assimilation for steps_per_da_window
+#    - assimilate first, then run eqn
+#    - a final assimilation for the end of window
+# 3) forward assimilation for steps_per_da_window
+#    - run first, then assimilate
+# Repeat from 1)
+#
+# Then save data after final loop
+
 for k in range(n_prediction_windows_spin_up, n_prediction_windows):
+    
+
     print_info("Prediction window: {}/{}".format(k+1, n_prediction_windows))
     timer_window = timer()
 
@@ -179,34 +193,40 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
                      k * prediction_window,
                      eps=static_contact_interval)
     current_time = k * prediction_window # to avoid build-up of errors
-
-    # START: prediction ########################################################
+    
+    ## 1) forward run w/o data assimilation; prediction
     master_eqn_ensemble.set_start_time(current_time)
-
     for j in range(steps_per_prediction_window):
-        master_states_timeseries.push_back(ensemble_state) # store
+        #Storage of data first (we do not store end of prediction window)
+        master_states_timeseries.push_back(ensemble_state)
+        
+        loaded_data = epidemic_data_storage.get_network_from_start_time(start_time=current_time)
 
-        loaded_data = epidemic_data_storage.get_network_from_start_time(
-                start_time=current_time)
         user_network.update_from(loaded_data.contact_network)
-        master_eqn_ensemble.set_mean_contact_duration(
-                user_network.get_edge_weights())
+        master_eqn_ensemble.set_mean_contact_duration(user_network.get_edge_weights())
 
-        ensemble_state = master_eqn_ensemble.simulate(
-                static_contact_interval,
-                min_steps=n_forward_steps)
+        # run ensemble forward
+        ensemble_state = master_eqn_ensemble.simulate(static_contact_interval,
+                                                      min_steps=n_forward_steps)
 
         master_eqn_ensemble.set_states_ensemble(ensemble_state)
 
         current_time += static_contact_interval
-    # END: prediction ##########################################################
-
-    # START: backward run with data assimilation ###############################
+    
     past_time = current_time
-    master_eqn_ensemble.set_start_time(past_time)
-
+    print_info("Prediction ended: current time:", current_time)
+    
+    ## 2) backward run with data assimilation
+    master_eqn_ensemble.set_start_time(current_time)
     for j in range(steps_per_da_window):
-        # data assimilation
+
+        #now load a run
+        loaded_data = epidemic_data_storage.get_network_from_end_time(end_time=past_time)
+
+        user_network.update_from(loaded_data.contact_network)
+        master_eqn_ensemble.set_mean_contact_duration(user_network.get_edge_weights())
+
+        #Assimilate first
         assimilate_I_now = modulo_is_close_to_zero(past_time,
                                                    I_assimilation_interval,
                                                    eps=static_contact_interval)
@@ -240,28 +260,19 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
                     past_time)
 
         # update ensemble after data assimilation
-        if (assimilate_I_now and delay_satisfied) or (assimilate_HD_now):
-            master_eqn_ensemble.set_states_ensemble(ensemble_state)
-            master_eqn_ensemble.update_ensemble(
-                    new_transition_rates=transition_rates_ensemble,
-                    new_transmission_rate=community_transmission_rate_ensemble)
+        master_eqn_ensemble.set_states_ensemble(ensemble_state)
+        master_eqn_ensemble.update_ensemble(
+                new_transition_rates=transition_rates_ensemble,
+                new_transmission_rate=community_transmission_rate_ensemble)
 
+        
         # run ensemble backwards
-        loaded_data = epidemic_data_storage.get_network_from_end_time(
-                end_time=past_time)
-
-        user_network.update_from(loaded_data.contact_network)
-        master_eqn_ensemble.set_mean_contact_duration(
-                user_network.get_edge_weights())
-
-        ensemble_state = master_eqn_ensemble.simulate_backwards(
-                static_contact_interval,
-                min_steps=n_backward_steps)
+        ensemble_state = master_eqn_ensemble.simulate_backwards(static_contact_interval,
+                                                                min_steps=n_backward_steps)
 
         past_time -= static_contact_interval
-    # END: backward run with data assimilation #################################
 
-    # START: one data assimilation update in the 'deepest' point ###############
+    #Final assimilation (at the peak of the sweep)
     assimilate_I_now = modulo_is_close_to_zero(past_time,
                                                I_assimilation_interval,
                                                eps=static_contact_interval)
@@ -272,50 +283,48 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
          transition_rates_ensemble,
          community_transmission_rate_ensemble
         ) = assimilator_imperfect_observations.update(
-                ensemble_state,
-                loaded_data.start_statuses,
-                transition_rates_ensemble,
-                community_transmission_rate_ensemble,
-                user_nodes,
-                past_time)
-
-    assimilate_HD_now = modulo_is_close_to_zero(past_time,
-                                                HD_assimilation_interval,
-                                                eps=static_contact_interval)
+            ensemble_state,
+            loaded_data.start_statuses,
+            transition_rates_ensemble,
+            community_transmission_rate_ensemble,
+            user_nodes,
+            past_time)
+        
+        assimilate_HD_now = modulo_is_close_to_zero(past_time,
+                                                    HD_assimilation_interval,
+                                                    eps=static_contact_interval)
     if assimilate_HD_now:
         (ensemble_state,
          transition_rates_ensemble,
          community_transmission_rate_ensemble
         ) = assimilator_perfect_observations.update(
-                ensemble_state,
-                loaded_data.start_statuses,
-                transition_rates_ensemble,
-                community_transmission_rate_ensemble,
-                user_nodes,
-                past_time)
+            ensemble_state,
+            loaded_data.start_statuses,
+            transition_rates_ensemble,
+            community_transmission_rate_ensemble,
+            user_nodes,
+            past_time)
 
-    if (assimilate_I_now and delay_satisfied) or (assimilate_HD_now):
-        master_eqn_ensemble.set_states_ensemble(ensemble_state)
-        master_eqn_ensemble.update_ensemble(
-                new_transition_rates=transition_rates_ensemble,
-                new_transmission_rate=community_transmission_rate_ensemble)
-    # END: one data assimilation update in the 'deepest' point #################
+    # update ensemble after data assimilation
+    master_eqn_ensemble.set_states_ensemble(ensemble_state)
+    master_eqn_ensemble.update_ensemble(
+        new_transition_rates=transition_rates_ensemble,
+        new_transmission_rate=community_transmission_rate_ensemble)
 
-    # START: forward run with data assimilation ################################
+    print_info("Backward assimilation ended; current time:", past_time)
+
+    ## 3) forward run with data assimilation
     master_eqn_ensemble.set_start_time(past_time)
 
     for j in range(steps_per_da_window):
-        # run ensemble forward
-        loaded_data = epidemic_data_storage.get_network_from_start_time(
-                start_time=past_time)
+        loaded_data = epidemic_data_storage.get_network_from_start_time(start_time=past_time)
 
         user_network.update_from(loaded_data.contact_network)
-        master_eqn_ensemble.set_mean_contact_duration(
-                user_network.get_edge_weights())
+        master_eqn_ensemble.set_mean_contact_duration(user_network.get_edge_weights())
 
-        ensemble_state = master_eqn_ensemble.simulate(
-                static_contact_interval,
-                min_steps=n_forward_steps)
+        # run ensemble forward
+        ensemble_state = master_eqn_ensemble.simulate(static_contact_interval,
+                                                      min_steps=n_forward_steps)
 
         past_time += static_contact_interval
 
@@ -353,34 +362,23 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
                     past_time)
 
         # update ensemble after data assimilation
-        if (assimilate_I_now and delay_satisfied) or (assimilate_HD_now):
-            master_eqn_ensemble.set_states_ensemble(ensemble_state)
-            master_eqn_ensemble.update_ensemble(
-                    new_transition_rates=transition_rates_ensemble,
-                    new_transmission_rate=community_transmission_rate_ensemble)
-    # END: forward run with data assimilation ##################################
+        master_eqn_ensemble.set_states_ensemble(ensemble_state)
+        master_eqn_ensemble.update_ensemble(
+                new_transition_rates=transition_rates_ensemble,
+                new_transmission_rate=community_transmission_rate_ensemble)
 
+    print_info("Forward assimilation ended; current time", past_time)
     # DA should get back to the current time
     assert are_close(past_time, current_time, eps=static_contact_interval)
-
+    
     print_info("Prediction window: {}/{};".format(k+1, n_prediction_windows),
-               "elapsed:", timer() - timer_window, end='\n\n')
+               "ended; elapsed:", timer() - timer_window, end='\n\n')
 
-master_states_timeseries.push_back(ensemble_state) # store after the last step
+## Final storage after last step
+master_states_timeseries.push_back(ensemble_state)
 
 
 # save & plot ##################################################################
-true_rates_tracker = PerformanceTracker()
-for j, kinetic_state in enumerate(kinetic_states_timeseries):
-    user_kinetic_state = dict_slice(kinetic_state, user_nodes)
-    true_rates_tracker.update(user_kinetic_state,
-                              master_states_timeseries.get_snapshot(j))
-
-np.save(os.path.join(OUTPUT_PATH, 'true_rates_track.npy'),
-        true_rates_tracker.performance_track)
-np.save(os.path.join(OUTPUT_PATH, 'time_span.npy'),
-        time_span)
-
 # plot trajectories
 axes = plot_ensemble_states(population,
                             master_states_timeseries.container,
@@ -392,18 +390,14 @@ plt.savefig(os.path.join(OUTPUT_PATH, 'epidemic_and_master_eqn.png'),
             rasterized=True,
             dpi=150)
 
-# plot true positive and negative rates
-_, axes_rates = plt.subplots()
-axes_rates.plot(time_span,
-                true_rates_tracker.performance_track[:,0],
-                label='true negative rate')
-axes_rates.plot(time_span,
-                true_rates_tracker.performance_track[:,1],
-                label='true positive rate')
+# save full the data we require:
+master_eqns_mean_states = master_states_timeseries.get_mean()
+np.save(os.path.join(OUTPUT_PATH, 'master_eqns_mean_states.npy'), master_eqns_mean_states)
 
-plt.legend()
-plt.savefig(os.path.join(OUTPUT_PATH, 'true_rates.png'),
-            rasterized=True,
-            dpi=150)
+kinetic_eqns_statuses = []
+for kinetic_state in kinetic_states_timeseries:
+    kinetic_eqns_statuses.append(dict_slice(kinetic_state, user_nodes))
+
+np.save(os.path.join(OUTPUT_PATH, 'kinetic_eqns_statuses.npy'), kinetic_eqns_statuses)
 
 