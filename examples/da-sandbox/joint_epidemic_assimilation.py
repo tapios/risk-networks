82180fe48757c4c344d6c3dfd836fa8cf069997a
odunbar <odunbar@caltech.edu>
2020-08-14 08:56:24 -0700

    added third assimilator to joint epidemic

--- examples/da-sandbox/joint_epidemic_assimilation.py
+++ examples/da-sandbox/joint_epidemic_assimilation.py
@@ -49,37 +49,50 @@ from _stochastic_init import (kinetic_ic,
 from _user_network_init import user_network, user_nodes, user_population
 
 # observations #################################################################
-from _observations_init import (random_infection_test,
-                                continuous_infection_test,
-                                budgeted_random_infection_test,
-                                neighbor_transfer_infection_test,
-                                high_var_infection_test,
+from _observations_init import (sensor_readings,
+                                MDT_neighbor_test,
+                                MDT_budget_random_test,
+                                MDT_result_delay,
+                                RDT_budget_random_test,
+                                RDT_result_delay,
                                 positive_hospital_records,
                                 negative_hospital_records,
                                 positive_death_records,
                                 negative_death_records)
 
-imperfect_observations = [continuous_infection_test,
-                          neighbor_transfer_infection_test]
+
+sensor_observations = [sensor_readings]
+
+viral_test_observations = [MDT_neighbor_test]
+test_result_delay = MDT_result_delay # delay to results of the virus test
+
 perfect_observations   = [positive_hospital_records,
                           negative_hospital_records,
                           positive_death_records,
                           negative_death_records]
 
-I_observation_delay = 0.0
 
 # assimilator ##################################################################
 transition_rates_to_update_str   = []
 transmission_rate_to_update_flag = False
 
-assimilator_imperfect_observations = DataAssimilator(
-        observations=imperfect_observations,
+
+sensor_assimilator = DataAssimilator(
+        observations=sensor_observations,
         errors=[],
         n_assimilation_batches = arguments.assimilation_batches_imperfect,
         transition_rates_to_update_str=transition_rates_to_update_str,
         transmission_rate_to_update_flag=transmission_rate_to_update_flag)
 
-assimilator_perfect_observations = DataAssimilator(
+viral_test_assimilator = DataAssimilator(
+        observations=viral_test_observations,
+        errors=[],
+        n_assimilation_batches = 1,
+        transition_rates_to_update_str=transition_rates_to_update_str,
+        transmission_rate_to_update_flag=transmission_rate_to_update_flag)
+
+
+record_assimilator = DataAssimilator(
         observations=perfect_observations,
         errors=[],
         n_assimilation_batches = arguments.assimilation_batches_perfect,
@@ -127,8 +140,9 @@ kinetic_states_timeseries.append(kinetic_state) # storing ic
 #floats
 da_window         = 7.0
 prediction_window = 1.0
-HD_assimilation_interval = 1.0 # assimilate H and D data every .. days
-I_assimilation_interval  = 1.0 # same for I
+record_assimilation_interval = 1.0 # assimilate H and D data every .. days
+test_assimilation_interval  = 1.0 # same for I
+sensor_assimilation_interval  = 1.0 # same for I
 
 intervention_start_time = arguments.intervention_start_time
 intervention_interval = arguments.intervention_interval
@@ -220,22 +234,35 @@ for j in range(spin_up_steps):
 
     #generate data to be assimilated later on
     if current_time > (earliest_assimilation_time - 0.1*static_contact_interval):
-        assimilate_I_now = modulo_is_close_to_zero(current_time,
-                                                   I_assimilation_interval,
+        observe_sensor_now = modulo_is_close_to_zero(current_time,
+                                                   sensor_assimilation_interval,
                                                    eps=static_contact_interval)
-        if assimilate_I_now:
-            print("gather data at ", current_time)
-            assimilator_imperfect_observations.find_and_store_observations(
+        if observe_sensor_now:
+            print("gather sensor data at ", current_time)
+            sensor_assimilator.find_and_store_observations(
                 ensemble_state,
                 loaded_data.end_statuses,
                 user_network,
                 current_time)
 
-        assimilate_HD_now = modulo_is_close_to_zero(current_time,
-                                                    HD_assimilation_interval,
+
+        observe_test_now = modulo_is_close_to_zero(current_time,
+                                                   test_assimilation_interval,
+                                                   eps=static_contact_interval)
+        if observe_test_now:
+            print("gather test data at ", current_time)
+            viral_test_assimilator.find_and_store_observations(
+                ensemble_state,
+                loaded_data.end_statuses,
+                user_network,
+                current_time)
+
+        observe_record_now = modulo_is_close_to_zero(current_time,
+                                                    record_assimilation_interval,
                                                     eps=static_contact_interval)
-        if assimilate_HD_now:
-            assimilator_perfect_observations.find_and_store_observations(
+        if observe_record_now:
+            print("gather records at ", current_time) 
+            record_assimilator.find_and_store_observations(
                 ensemble_state,
                 loaded_data.end_statuses,
                 user_network,
@@ -246,9 +273,7 @@ for j in range(spin_up_steps):
     if intervention_frequency == "none":
         intervene_now=False
     elif intervention_frequency == "single":
-        intervene_now = modulo_is_close_to_zero(current_time,
-                                                intervention_start_time,
-                                                eps=static_contact_interval)
+        intervene_now = (abs(current_time - intervention_start_time) < 0.1*static_contact_interval)
     elif intervention_frequency == "interval":
         if current_time > intervention_start_time - 0.1 * static_contact_interval:
             intervene_now = modulo_is_close_to_zero(current_time,
@@ -357,23 +382,39 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
                                                       min_steps=n_forward_steps)
         current_time += static_contact_interval
 
-        assimilate_I_now = modulo_is_close_to_zero(current_time,
-                                                   I_assimilation_interval,
-                                                   eps=static_contact_interval)
-        if assimilate_I_now:
-            print("gather data at ", current_time)
 
-            assimilator_imperfect_observations.find_and_store_observations(
+        # collect data for later assimilation
+        observe_sensor_now = modulo_is_close_to_zero(current_time,
+                                                     sensor_assimilation_interval,
+                                                     eps=static_contact_interval)
+
+        if observe_sensor_now:
+            print("gather sensor data at ", current_time)
+            sensor_assimilator.find_and_store_observations(
                 ensemble_state,
                 loaded_data.end_statuses,
                 user_network,
                 current_time)
 
-        assimilate_HD_now = modulo_is_close_to_zero(current_time,
-                                                    HD_assimilation_interval,
+
+        observe_test_now = modulo_is_close_to_zero(current_time,
+                                                   test_assimilation_interval,
+                                                   eps=static_contact_interval)
+        if observe_test_now:
+            print("gather test data at ", current_time)
+
+            viral_test_assimilator.find_and_store_observations(
+                ensemble_state,
+                loaded_data.end_statuses,
+                user_network,
+                current_time)
+
+        observe_record_now = modulo_is_close_to_zero(current_time,
+                                                    record_assimilation_interval,
                                                     eps=static_contact_interval)
-        if assimilate_HD_now:
-            assimilator_perfect_observations.find_and_store_observations(
+        if observe_record_now:
+            print("gather records at ", current_time) 
+            record_assimilator.find_and_store_observations(
                 ensemble_state,
                 loaded_data.end_statuses,
                 user_network,
@@ -392,31 +433,46 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
             loaded_data = epidemic_data_storage.get_network_from_end_time(end_time=past_time)
 
             # data assimilation
-            assimilate_I_now = modulo_is_close_to_zero(past_time,
-                                                       I_assimilation_interval,
-                                                       eps=static_contact_interval)
-
-            delay_satisfied = past_time <= (current_time - I_observation_delay)
-
-            if assimilate_I_now and delay_satisfied:
+            assimilate_sensor_now = modulo_is_close_to_zero(past_time,
+                                                            sensor_assimilation_interval,
+                                                            eps=static_contact_interval)
+            
+            if assimilate_sensor_now:
                 (ensemble_state,
                  transition_rates_ensemble,
                  community_transmission_rate_ensemble
-                ) = assimilator_imperfect_observations.update(
+                ) = sensor_assimilator.update(
                         ensemble_state,
                         loaded_data.end_statuses,
                         transition_rates_ensemble,
                         community_transmission_rate_ensemble,
                         past_time)
 
-            assimilate_HD_now = modulo_is_close_to_zero(past_time,
-                                                        HD_assimilation_interval,
-                                                        eps=static_contact_interval)
-            if assimilate_HD_now:
+            assimilate_test_now = modulo_is_close_to_zero(past_time,
+                                                       test_assimilation_interval,
+                                                       eps=static_contact_interval)
+
+            delay_satisfied = past_time <= (current_time - test_result_delay)
+
+            if assimilate_test_now and delay_satisfied:
                 (ensemble_state,
                  transition_rates_ensemble,
                  community_transmission_rate_ensemble
-                ) = assimilator_perfect_observations.update(
+                ) = viral_test_assimilator.update(
+                        ensemble_state,
+                        loaded_data.end_statuses,
+                        transition_rates_ensemble,
+                        community_transmission_rate_ensemble,
+                        past_time)
+
+            assimilate_record_now = modulo_is_close_to_zero(past_time,
+                                                        record_assimilation_interval,
+                                                        eps=static_contact_interval)
+            if assimilate_record_now:
+                (ensemble_state,
+                 transition_rates_ensemble,
+                 community_transmission_rate_ensemble
+                ) = record_assimilator.update(
                         ensemble_state,
                         loaded_data.end_statuses,
                         transition_rates_ensemble,
@@ -424,7 +480,7 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
                         past_time)
 
             # update ensemble after data assimilation
-            if (assimilate_I_now and delay_satisfied) or (assimilate_HD_now):
+            if (assimilate_test_now and delay_satisfied) or (assimilate_record_now):
                 master_eqn_ensemble.set_states_ensemble(ensemble_state)
                 master_eqn_ensemble.update_ensemble(
                         new_transition_rates=transition_rates_ensemble,
@@ -439,30 +495,45 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
             past_time -= static_contact_interval
 
         # furthest-in-the-past assimilation (at the peak of the sweep)
-        assimilate_I_now = modulo_is_close_to_zero(past_time,
-                                                   I_assimilation_interval,
-                                                   eps=static_contact_interval)
-        delay_satisfied = past_time <= (current_time - I_observation_delay)
-
-        if assimilate_I_now and delay_satisfied:
+        assimilate_sensor_now = modulo_is_close_to_zero(past_time,
+                                                        sensor_assimilation_interval,
+                                                        eps=static_contact_interval)
+            
+        if assimilate_sensor_now:
             (ensemble_state,
              transition_rates_ensemble,
              community_transmission_rate_ensemble
-            ) = assimilator_imperfect_observations.update(
+            ) = sensor_assimilator.update(
+                ensemble_state,
+                loaded_data.end_statuses,
+                transition_rates_ensemble,
+                community_transmission_rate_ensemble,
+                past_time)
+
+        assimilate_test_now = modulo_is_close_to_zero(past_time,
+                                                   test_assimilation_interval,
+                                                   eps=static_contact_interval)
+        delay_satisfied = past_time <= (current_time - test_result_delay)
+
+        if assimilate_test_now and delay_satisfied:
+            (ensemble_state,
+             transition_rates_ensemble,
+             community_transmission_rate_ensemble
+            ) = viral_test_assimilator.update(
                 ensemble_state,
                 loaded_data.start_statuses,
                 transition_rates_ensemble,
                 community_transmission_rate_ensemble,
                 past_time)
             
-        assimilate_HD_now = modulo_is_close_to_zero(past_time,
-                                                    HD_assimilation_interval,
+        assimilate_record_now = modulo_is_close_to_zero(past_time,
+                                                    record_assimilation_interval,
                                                     eps=static_contact_interval)
-        if assimilate_HD_now:
+        if assimilate_record_now:
             (ensemble_state,
              transition_rates_ensemble,
              community_transmission_rate_ensemble
-            ) = assimilator_perfect_observations.update(
+            ) = record_assimilator.update(
                 ensemble_state,
                 loaded_data.start_statuses,
                 transition_rates_ensemble,
@@ -470,7 +541,7 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
                 past_time)
 
         # update ensemble after data assimilation
-        if (assimilate_I_now and delay_satisfied) or (assimilate_HD_now):
+        if (assimilate_test_now and delay_satisfied) or (assimilate_record_now):
             master_eqn_ensemble.set_states_ensemble(ensemble_state)
             master_eqn_ensemble.update_ensemble(
                 new_transition_rates=transition_rates_ensemble,
@@ -493,30 +564,45 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
             past_time += static_contact_interval
 
             # data assimilation
-            assimilate_I_now = modulo_is_close_to_zero(past_time,
-                                                       I_assimilation_interval,
-                                                       eps=static_contact_interval)
-            delay_satisfied = past_time <= (current_time - I_observation_delay)
-
-            if assimilate_I_now and delay_satisfied:
+            assimilate_sensor_now = modulo_is_close_to_zero(past_time,
+                                                            sensor_assimilation_interval,
+                                                            eps=static_contact_interval)
+            
+            if assimilate_sensor_now:
                 (ensemble_state,
                  transition_rates_ensemble,
                  community_transmission_rate_ensemble
-                ) = assimilator_imperfect_observations.update(
+                ) = sensor_assimilator.update(
+                    ensemble_state,
+                    loaded_data.end_statuses,
+                    transition_rates_ensemble,
+                    community_transmission_rate_ensemble,
+                    past_time)
+
+            assimilate_test_now = modulo_is_close_to_zero(past_time,
+                                                       test_assimilation_interval,
+                                                       eps=static_contact_interval)
+            delay_satisfied = past_time <= (current_time - test_result_delay)
+
+            if assimilate_test_now and delay_satisfied:
+                (ensemble_state,
+                 transition_rates_ensemble,
+                 community_transmission_rate_ensemble
+                ) = viral_test_assimilator.update(
                         ensemble_state,
                         loaded_data.end_statuses,
                         transition_rates_ensemble,
                         community_transmission_rate_ensemble,
                         past_time)
 
-            assimilate_HD_now = modulo_is_close_to_zero(past_time,
-                                                        HD_assimilation_interval,
+            assimilate_record_now = modulo_is_close_to_zero(past_time,
+                                                        record_assimilation_interval,
                                                         eps=static_contact_interval)
-            if assimilate_HD_now:
+            if assimilate_record_now:
                 (ensemble_state,
                  transition_rates_ensemble,
                  community_transmission_rate_ensemble
-                ) = assimilator_perfect_observations.update(
+                ) = record_assimilator.update(
                         ensemble_state,
                         loaded_data.end_statuses,
                         transition_rates_ensemble,
@@ -524,7 +610,7 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
                         past_time)
 
             # update ensemble after data assimilation
-            if (assimilate_I_now and delay_satisfied) or (assimilate_HD_now):
+            if (assimilate_test_now and delay_satisfied) or (assimilate_record_now):
                 master_eqn_ensemble.set_states_ensemble(ensemble_state)
                 master_eqn_ensemble.update_ensemble(
                         new_transition_rates=transition_rates_ensemble,
@@ -543,9 +629,7 @@ for k in range(n_prediction_windows_spin_up, n_prediction_windows):
     if intervention_frequency == "none":
         intervene_now=False
     elif intervention_frequency == "single":
-        intervene_now = modulo_is_close_to_zero(current_time,
-                                                intervention_start_time,
-                                                eps=static_contact_interval)
+        intervene_now = (abs(current_time - intervention_start_time) < 0.1*static_contact_interval)
     elif intervention_frequency == "interval":
         if current_time > intervention_start_time - 0.1 * static_contact_interval:
             intervene_now = modulo_is_close_to_zero(current_time,
