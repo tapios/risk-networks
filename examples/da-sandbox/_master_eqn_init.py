1c902b7f1508f02873c9943833694b789e7bb9c6
Jinlong Wu <jinlongwu@Jinlongs-MacBook-Pro.local>
2020-08-19 14:43:31 -0700

    added prior distributions of transition rates.

--- examples/da-sandbox/_master_eqn_init.py
+++ examples/da-sandbox/_master_eqn_init.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pdb
 
 from _argparse_init import arguments
 
@@ -18,6 +17,10 @@ from _stochastic_init import transition_rates
 from _user_network_init import user_nodes, user_population
 from _utilities import print_start_of, print_end_of
 
+from epiforecast.populations import TransitionRates
+from epiforecast.samplers import BetaSampler, GammaSampler
+from _network_init import network
+
 
 print_start_of(__name__)
 ################################################################################
@@ -25,21 +28,34 @@ ensemble_size = 100
 n_forward_steps  = 1 # minimum amount of steps per time step: forward run
 n_backward_steps = 5 # minimum amount of steps per time step: backward run
 
+# Prior of transition rates ####################################################
 learn_transition_rates = arguments.learn_transition_rates
 parameter_str = arguments.transition_rates_str.split(',') 
 noise_level = [float(value) for value in arguments.transition_rates_noise.split(',')]
 transition_rates_ensemble = []
-for i in range(ensemble_size):
-    transition_rates_particle = transition_rates[user_nodes]
-    if learn_transition_rates == True:
-        transition_rates_particle.add_noise_to_clinical_parameters(
-                parameter_str, noise_level)
-    transition_rates_particle.calculate_from_clinical()
-    transition_rates_ensemble.append(transition_rates_particle)
+if learn_transition_rates == True:
+    for i in range(ensemble_size):
+        transition_rates = TransitionRates.from_samplers(
+                population=network.get_node_count(),
+                lp_sampler=GammaSampler(1.7,2.,1.),
+                cip_sampler=GammaSampler(1.5,2.,1.),
+                hip_sampler=GammaSampler(1.5,3.,1.),
+                hf_sampler=BetaSampler(4.,0.036),
+                cmf_sampler=BetaSampler(4.,0.001),
+                hmf_sampler=BetaSampler(4.,0.18)
+        )
+        transition_rates.calculate_from_clinical()
+        transition_rates_particle = transition_rates[user_nodes]
+        transition_rates_ensemble.append(transition_rates_particle)
 
+else:
+    for i in range(ensemble_size):
+        transition_rates_particle = transition_rates[user_nodes]
+        transition_rates_ensemble.append(transition_rates_particle)
+
+# Prior of transmission rate ###################################################
 community_transmission_rate_ensemble = np.full([ensemble_size, 1],
                                                community_transmission_rate)
-
 learn_transmission_rate = arguments.learn_transmission_rate
 transmission_rate_bias = arguments.transmission_rate_bias
 transmission_rate_std = arguments.transmission_rate_noise * community_transmission_rate
