4188719d651018669ab2c07bb2a43e3be1883468
odunbar <odunbar@caltech.edu>
2020-08-21 06:45:18 -0700

    added possibility to change observation budgets over time

--- examples/da-sandbox/_observations_init.py
+++ examples/da-sandbox/_observations_init.py
@@ -17,8 +17,7 @@ sensor_wearers=np.random.choice(user_nodes, size=arguments.observations_sensor_w
 
 
 # imperfect observations #######################################################
-# sensor type observations
-
+# sensor type observation
 sensor_readings = FixedObservation(
     N=user_population,
     obs_nodes=sensor_wearers,
@@ -55,19 +54,52 @@ MDT_budget_random_test = BudgetedObservation(
         specificity=0.99,
         obs_var_min=1e-6)
 
+MDT_high_var_test = HighVarianceObservation(
+        N=user_population,
+        obs_frac=arguments.observations_I_fraction_tested,
+        obs_status='I',
+        obs_name="Test maximal variance infected MDT",
+        noisy_measurement=True,
+        sensitivity=0.95,
+        specificity=0.99,
+        obs_var_min=1e-6)
+
 # Rapid Diagnostic Test
 RDT_result_delay = 0.0 # delay to results of the virus test
 RDT_budget_random_test = BudgetedObservation(
         N=user_population,
         obs_budget=arguments.observations_I_budget,
         obs_status='I',
-        obs_name="Budgeted Infection Test",
+        obs_name="85% sensitive Budgeted RDT",
         min_threshold=arguments.observations_I_min_threshold,
         max_threshold=arguments.observations_I_max_threshold,
         noisy_measurement=True,
         sensitivity=0.85,
         specificity=0.99,
         obs_var_min=1e-6)
+
+poor_RDT_budget_random_test = BudgetedObservation(
+        N=user_population,
+        obs_budget=arguments.observations_I_budget,
+        obs_status='I',
+        obs_name="60% sensitive Budgeted RDT",
+        min_threshold=arguments.observations_I_min_threshold,
+        max_threshold=arguments.observations_I_max_threshold,
+        noisy_measurement=True,
+        sensitivity=0.6,
+        specificity=0.99,
+        obs_var_min=1e-6)
+
+RDT_high_var_test = HighVarianceObservation(
+        N=user_population,
+        obs_frac=arguments.observations_I_fraction_tested,
+        obs_status='I',
+        obs_name="Test maximal variance infected RDT",
+        noisy_measurement=True,
+        sensitivity=0.85,
+        specificity=0.99,
+        obs_var_min=1e-6)
+
 
 # generic test templates
 continuous_infection_test = FixedObservation(
@@ -150,6 +182,36 @@ negative_death_records = DataObservation(
         obs_status='D',
         obs_name="negative_death_records")
 
+
+
+################################################################################
+def transition_function(x, y, transition_steps, total_steps):
+    """
+    linear transition, stays at final value
+    f(0) = x, 
+    f(transition_steps) = y,
+    f(transition_steps + k ) = y 
+
+    Args
+    ----
+    x (float): initial value
+    y (float): final value
+    transition_steps (int): number of steps to transition over
+    total_steps 
+
+    """
+    assert (total_steps >= transition_steps)
+    output=np.zeros(total_steps)
+    T=transition_steps-1
+    output[:T] = np.array([ (T - step)/T * x + step/T * y for step in range(T)])
+    output[T:] = y
+    
+    return output
+
+
+
+
 ################################################################################
 print_end_of(__name__)
+
 
