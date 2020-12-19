9d088a8e394beb47eef29ea008563ad8a8050eda
odunbar <odunbar@caltech.edu>
2020-12-19 07:28:28 -0800

    added a tanh

--- epiforecast/transforms.py
+++ epiforecast/transforms.py
@@ -4,27 +4,36 @@ class Transform:
 
     def __init__(
             self,
-            name="identity"):   
+            name="identity",
+            **kwargs):   
         '''
         Instantiate the object to implement transforms defined by a name
         
         Args
         ----
-        name (string): "identity", "logit"
+        name (string), extra parameters,
+        "identity",    None
+        "logit",       None
+        "tanh",        lengthscale
+                       
         
         '''
         self.name = name
+        if self.name == "tanh":
+            self.lengthscale = kwargs.get('lengthscale',1) #1 is a default lengthscale
     
     def apply_transform(self,x):
         return {   
             'identity' : lambda x: x,
-            'logit'    : lambda x:  np.log(np.maximum(x, 1e-9) / np.maximum(1.0 - x, 1e-9))
+            'logit'    : lambda x: np.log(np.maximum(x, 1e-9) / np.maximum(1.0 - x, 1e-9))
+            'tanh'     : lambda x: np.arctanh(2*x - 1)*self.lengthscale
         }[self.name](x)
 
     def apply_inverse_transform(self,x):
         return {
             'identity' : lambda x: x,
-            'logit'    : lambda x: np.exp(x_logit)/(np.exp(x_logit) + 1.0)
+            'logit'    : lambda x: np.exp(x)/(np.exp(x) + 1.0)
+            'tanh'     : lambda x: 1 + 0.5*np.tanh(x/self.lengthscale), 
         }[self.name](x)
 
         
