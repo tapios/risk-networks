ed5a9de0148c5aa53b488ca3ed28a6623906d78e
odunbar <odunbar@caltech.edu>
2021-01-12 13:21:38 -0800

    new 100k expt

--- examples/da-sandbox/run_expt_user100_nosensors.sh
+++ examples/da-sandbox/run_expt_user100_nosensors.sh
@@ -30,9 +30,7 @@ echo "requested ${num_cpus} cores and ray is told ${bytes_of_memory} memory avai
 EXP_NAME="NYC_1e5_user100_nosensors" #1e5 = 97942 nodes
 network_size=1e5
 wearers=0
-batches_sensors=1
-batches_records=392 #195884 nodes
-
+# note: 195884 nodes
 
 # user base
 user_fraction=1.0
@@ -45,15 +43,32 @@ I_max_threshold=1.0
 int_freq='single'
 intervention_start_time=18
 
-# other
-update_test="local"
+#da params 
+da_window=1.0
+n_sweeps=1
+
+#observation_noise
+obs_noise=1e-8
+#reglarization parameters
+test_reg=1e-1
+record_reg=1e-2
+
+# observation localization (number of nbhds observations have effect on (integer) 0 = delta at observation, 1= nbhd of observation)
+distance_threshold=1
+
+#inflation (No inflation is 1.0)
+test_inflation=2.0
+record_inflation=5.0
+
+#param noise mean - ln((mu+bias)^2 / sqrt((mu+bias)^2 +sig^2))
+param_prior_noise=0.2
+param_prior_bias=-4
+
 
 # Experimental series parameters ###############################################
-#1% 5% 25% of 97942
-test_budgets=(979 4897 24486)  
+#5% 10% 25% of 97942
+test_budgets=(0 4897 9794 24486)  
 budget=${test_budgets[${SLURM_ARRAY_TASK_ID}]}
-batches_tests=(2 10 50) #so no batch > 1000 nodes
-batches_test=${batches_tests[${SLURM_ARRAY_TASK_ID}]}
 
 # output parameters
 OUTPUT_DIR="output"
@@ -63,25 +78,34 @@ stderr="${output_path}/stderr"
 
 mkdir -p "${output_path}"
 
+echo "output to be found in: ${output_path}, stdout in $stdout, stderr in $stderr "
+
 
 # launch #######################################################################
-python3 joint_epidemic_assimilation.py \
+# launch #######################################################################
+python3 joint_iterated_forward_assimilation.py \
   --user-network-user-fraction=${user_fraction} \
   --constants-output-path=${output_path} \
+  --observations-noise=${obs_noise} \
   --observations-I-budget=${budget} \
-  --observations-I-min-threshold=${I_min_threshold} \
-  --observations-I-max-threshold=${I_max_threshold} \
   --observations-sensor-wearers=${wearers} \
   --network-node-count=${network_size} \
-  --assimilation-batches-sensor=${batches_sensors} \
-  --assimilation-batches-test=${batches_tests} \
-  --assimilation-batches-record=${batches_records} \
   --parallel-flag \
   --parallel-memory=${bytes_of_memory} \
   --parallel-num-cpus=${num_cpus} \
   --intervention-frequency=${int_freq} \
   --intervention-start-time=${intervention_start_time} \
-  --assimilation-update-test=${update_test} \
+  --test-assimilation-joint-regularization=${test_reg} \
+  --record-assimilation-joint-regularization=${record_reg} \
+  --assimilation-test-inflation=${test_inflation} \
+  --assimilation-record-inflation=${record_inflation} \
+  --distance-threshold=${distance_threshold} \
+  --assimilation-window=${da_window} \
+  --assimilation-sweeps=${n_sweeps} \
   >${stdout} 2>${stderr}
+#  --params-learn-transmission-rate \
+#  --params-transmission-rate-noise=${param_prior_noise} \
+#  --params-transmission-rate-bias=${param_prior_bias} \
+
 
 
