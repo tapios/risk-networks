a887bf9347d8ec87c1ea00f9368816a874d40552
odunbar <odunbar@caltech.edu>
2021-04-01 11:39:35 -0700

    new run length

--- examples/da-sandbox/_constants.py
+++ examples/da-sandbox/_constants.py
@@ -45,7 +45,7 @@ static_contact_interval = 3 * hour
 mean_contact_lifetime = 2 * minute
 
 start_time  = 0.0   # the ultimate start time, i.e. when the simulation starts
-end_time    = 90.0  # the ultimate end time
+end_time    = 127.0  # the ultimate end time
 total_time  = end_time - start_time
 total_steps = int(total_time/static_contact_interval)
 
